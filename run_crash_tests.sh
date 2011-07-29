0a19c5823db4b7c3a743019bcb174d7793f09f7f
Vince Weaver <vweaver1@eecs.utk.edu>
2011-07-28 23:29:00 -0400

    + Add oflo_sw_cpu_clock_crash crash code + Release version 0.19

--- run_crash_tests.sh
+++ run_crash_tests.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
 echo
-echo "I'm not going to automate this."
-echo "If you want to run the crash tests"
-echo "then cd into crashes and run them"
-echo "by hand."
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "            WARNING               "
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo
+echo "These tests can crash your kernel!"
+echo "Only run if you have properly patched kernels!"
+echo "Be sure to sync your disk first too."
+echo
+echo "Waiting 5 seconds before running to give you "
+echo "time to change your mind..."
+echo
+sleep 5
 
-
+./crashes/pe_task_schedule_panic
+./crashes/pe_inherit_memleak
+./crashes/oflo_sw_cpu_clock_crash
