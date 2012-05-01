1b49acff0baabaa663080dae03bc768dd8446a93
Vince Weaver <vweaver1@eecs.utk.edu>
2012-05-01 15:47:55 -0400

    Enhance the validation/non-existent test
    It was wrongly using PERF_TYPE_HARDWARE where it should have
    been using PERF_TYPE_HW_CACHE.
    
    Also extend it to cover more processors.
    
    Thanks to Sam Wang for noticing the latter problem.

--- validation/non-existent.c
+++ validation/non-existent.c
@@ -1,5 +1,10 @@
 /* This file attempts to test if non-existent events fail */
 
+/* The reason for this test is that in the various linux-kernel  */
+/*   pre-defined event definitions, sometimes unavailable events */
+/*   are implicitly set to 0 and sometimes set to -1.            */
+/*   This test makes sure both fail properly.                    */
+
 /* by Vince Weaver, vweaver1 _at_ eecs.utk.edu        */
 
 
@@ -24,19 +29,77 @@ int main(int argc, char **argv) {
 
    struct perf_event_attr pe;
    int fails=0,expected_fails=0,cpu;
-   int zero_event,negone_event;
+   int zero_event=0,negone_event=0;
+   int zero_type=0,negone_type=0;
 
    quiet=test_quiet();
 
    cpu=detect_processor();
+
+   /* TODO:
+        PROCESSOR_PENTIUM_PRO  1
+        PROCESSOR_PENTIUM_II   2
+        PROCESSOR_PENTIUM_III  3
+        PROCESSOR_PENTIUM_4    4
+        PROCESSOR_PENTIUM_M    5
+        PROCESSOR_COREDUO      6
+        PROCESSOR_AMD_FAM15H  19
+        PROCESSOR_POWER3      103
+        PROCESSOR_POWER4      104
+        PROCESSOR_POWER5      105
+        PROCESSOR_POWER6      106
+        PROCESSOR_POWER7      107
+   */
+
+   if (( cpu==PROCESSOR_CORTEX_A8) ||
+       ( cpu==PROCESSOR_CORTEX_A9)) {
+
+     /* ARM does things a bit differently; it has HW_OP_UNSUPPORTED, */
+     /* and CACHE_OP_UNSUPPORTED, bot set to 0xffff                  */
+
+     /* HW_OP_UNSUPPORTED */
+     zero_type=PERF_TYPE_HARDWARE;
+     zero_event=PERF_COUNT_HW_BUS_CYCLES;
+
+     /* CACHE_OP_UNSUPPORTED */
+     negone_type=PERF_TYPE_HW_CACHE;
+     negone_event= PERF_COUNT_HW_CACHE_BPU |
+                   ( PERF_COUNT_HW_CACHE_OP_PREFETCH <<8) |
+                   (PERF_COUNT_HW_CACHE_RESULT_ACCESS <<16);
+
+   }
+
+   else if (( cpu==PROCESSOR_SANDYBRIDGE) ||
+       ( cpu==PROCESSOR_WESTMERE) ||
+       ( cpu==PROCESSOR_WESTMERE_EX) ||
+       ( cpu==PROCESSOR_NEHALEM) ||
+       ( cpu==PROCESSOR_NEHALEM_EX) ||
+       ( cpu== PROCESSOR_CORE2) ||
+       ( cpu== PROCESSOR_ATOM)) {
+
+      zero_type=PERF_TYPE_HW_CACHE;
+      zero_event= PERF_COUNT_HW_CACHE_DTLB |
+                   ( PERF_COUNT_HW_CACHE_OP_PREFETCH <<8) |
+                   (PERF_COUNT_HW_CACHE_RESULT_ACCESS <<16);
+
+      negone_type=PERF_TYPE_HW_CACHE;
+      negone_event= PERF_COUNT_HW_CACHE_BPU |
+                   ( PERF_COUNT_HW_CACHE_OP_PREFETCH <<8) |
+                   (PERF_COUNT_HW_CACHE_RESULT_ACCESS <<16);
+   }
    
-   if ( (cpu==PROCESSOR_K7) ||
+   else if ( (cpu==PROCESSOR_K7) ||
         (cpu==PROCESSOR_K8) ||
         (cpu==PROCESSOR_AMD_FAM10H) ||
         (cpu==PROCESSOR_AMD_FAM11H) ||
         (cpu==PROCESSOR_AMD_FAM14H)) {
 
+     /* event implicitly set to 0 */ 
+     zero_type=PERF_TYPE_HARDWARE;
      zero_event=PERF_COUNT_HW_BUS_CYCLES;
+
+     /* event set to -1 in linux-2.6/arch/x86/kernel/cpu/perf_event_amd.h */
+     negone_type=PERF_TYPE_HW_CACHE;
      negone_event= PERF_COUNT_HW_CACHE_BPU |
                    ( PERF_COUNT_HW_CACHE_OP_PREFETCH <<8) |
                    (PERF_COUNT_HW_CACHE_RESULT_ACCESS <<16);
@@ -52,7 +115,7 @@ int main(int argc, char **argv) {
    }
 
    memset(&pe,0,sizeof(struct perf_event_attr));
-   pe.type=PERF_TYPE_HARDWARE;
+   pe.type=zero_type;
    pe.size=sizeof(struct perf_event_attr);
    pe.config=zero_event;
    pe.disabled=1;
@@ -77,7 +140,7 @@ int main(int argc, char **argv) {
    }
 
    memset(&pe,0,sizeof(struct perf_event_attr));
-   pe.type=PERF_TYPE_HARDWARE;
+   pe.type=negone_type;
    pe.size=sizeof(struct perf_event_attr);
 
    pe.config=negone_event;
