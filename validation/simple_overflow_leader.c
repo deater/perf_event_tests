4939af9104c88d54defa0274552b728c15f8e2aa
Vince Weaver <vince@deater.net>
2012-11-24 15:33:32 -0500

    Make simple_overflow_leader behavior actually match name.

--- validation/simple_overflow_leader.c
+++ validation/simple_overflow_leader.c
@@ -37,7 +37,7 @@ static int fd1;
 static void our_handler(int signum,siginfo_t *oh, void *blah) {
   int ret;
 
-  ret=ioctl(fd1, PERF_EVENT_IOC_DISABLE, 0);
+//  ret=ioctl(fd1, PERF_EVENT_IOC_DISABLE, 0);
 
   switch(oh->si_code) {
      case POLL_IN:  count.in++;  break;
@@ -51,41 +51,16 @@ static void our_handler(int signum,siginfo_t *oh, void *blah) {
 
   count.total++;
 
-  ret=ioctl(fd1, PERF_EVENT_IOC_REFRESH,1);
+//  ret=ioctl(fd1, PERF_EVENT_IOC_REFRESH,1);
 
   (void) ret;
   
 }
 
-static void our_handler2(int signum,siginfo_t *oh, void *blah) {
-  int ret;
-
-  ret=ioctl(fd1, PERF_EVENT_IOC_DISABLE, 0);
-
-  switch(oh->si_code) {
-     case POLL_IN:  count.in++;  break;
-     case POLL_OUT: count.out++; break;
-     case POLL_MSG: count.msg++; break;
-     case POLL_ERR: count.err++; break;
-     case POLL_PRI: count.pri++; break;
-     case POLL_HUP: count.hup++; break;
-     default: count.unknown++; break;
-  }
-
-  count.total++;
-
-  //  ret=ioctl(fd1, PERF_EVENT_IOC_ENABLE,0);
-  //  ret=ioctl(fd1, PERF_EVENT_IOC_REFRESH,3);
-
-  (void) ret;
- 
-}
-
-
 
 int main(int argc, char** argv) {
    
-   int ret,quiet,first_count,second_count;   
+   int ret,quiet;   
    int i;
 
    struct perf_event_attr pe;
@@ -119,7 +94,7 @@ int main(int argc, char** argv) {
    pe.pinned=1;
    pe.exclude_kernel=1;
    pe.exclude_hv=1;
-   pe.wakeup_events=1;
+//   pe.wakeup_events=1;
 
    arch_adjust_domain(&pe,quiet);
 
@@ -139,7 +114,7 @@ int main(int argc, char** argv) {
    
    ioctl(fd1, PERF_EVENT_IOC_RESET, 0);   
 
-   ret=ioctl(fd1, PERF_EVENT_IOC_REFRESH,1);
+   ret=ioctl(fd1, PERF_EVENT_IOC_ENABLE, 0);
 
    if (ret<0) {
      if (!quiet) fprintf(stderr,"Error with PERF_EVENT_IOC_ENABLE of group leader: "
@@ -150,6 +125,8 @@ int main(int argc, char** argv) {
    for (i=0;i<100;i++) {
        instructions_million();
    }
+   
+   ret=ioctl(fd1, PERF_EVENT_IOC_DISABLE, 0);   
    
    if (!quiet) {
       printf("Counts, using mmap buffer %p\n",our_mmap);
@@ -162,102 +139,25 @@ int main(int argc, char** argv) {
       printf("\tUNKNOWN : %d\n",count.unknown);
    }
 
-   if (count.total==0) {
-      if (!quiet) printf("No overflow events generated.\n");
-      test_fail(test_string);
-   }
-   first_count=count.total;
-
-   memset(&count,0,sizeof(count));
-
    close(fd1);
-
-   /****************************/
-   /* re set-up, but count / 3 */
-   /****************************/
-
-   memset(&sa, 0, sizeof(struct sigaction));
-   sa.sa_sigaction = our_handler2;
-   sa.sa_flags = SA_SIGINFO;
-
-   if (sigaction( SIGIO, &sa, NULL) < 0) {
-     fprintf(stderr,"Error setting up signal handler\n");
-     exit(1);
-   }
-
-   memset(&pe,0,sizeof(struct perf_event_attr));
-
-   pe.type=PERF_TYPE_HARDWARE;
-   pe.size=sizeof(struct perf_event_attr);
-   pe.config=PERF_COUNT_HW_INSTRUCTIONS;
-   pe.sample_period=100000;
-   pe.sample_type=PERF_SAMPLE_IP;
-   pe.read_format=PERF_FORMAT_GROUP|PERF_FORMAT_ID;
-   pe.disabled=1;
-   pe.pinned=1;
-   pe.exclude_kernel=1;
-   pe.exclude_hv=1;
-   pe.wakeup_events=3;
-
-   arch_adjust_domain(&pe,quiet);
-
-   fd1=perf_event_open(&pe,0,-1,-1,0);
-   if (fd1<0) {
-      if (!quiet) fprintf(stderr,"Error opening leader %llx\n",pe.config);
-      test_fail(test_string);
-   }
-
-   our_mmap=mmap(NULL, (1+1)*4096, 
-         PROT_READ|PROT_WRITE, MAP_SHARED, fd1, 0);
-
    
-   fcntl(fd1, F_SETFL, O_RDWR|O_NONBLOCK|O_ASYNC);
-   fcntl(fd1, F_SETSIG, SIGIO);
-   fcntl(fd1, F_SETOWN,getpid());
-   
-   ioctl(fd1, PERF_EVENT_IOC_RESET, 0);   
-
-   //   ret=ioctl(fd1, PERF_EVENT_IOC_ENABLE, 0);
-
-   ret=ioctl(fd1, PERF_EVENT_IOC_REFRESH, 10);
-
-   if (ret<0) {
-     if (!quiet) fprintf(stderr,"Error with PERF_EVENT_IOC_ENABLE of group leader: "
-	     "%d %s\n",errno,strerror(errno));
-     exit(1);
-   }
-
-   for (i=0;i<100;i++) {
-       instructions_million();
-   }
-    
-   if (!quiet) {
-      printf("Counts, using mmap buffer %p\n",our_mmap);
-      printf("\tPOLL_IN : %d\n",count.in);
-      printf("\tPOLL_OUT: %d\n",count.out);
-      printf("\tPOLL_MSG: %d\n",count.msg);
-      printf("\tPOLL_ERR: %d\n",count.err);
-      printf("\tPOLL_PRI: %d\n",count.pri);
-      printf("\tPOLL_HUP: %d\n",count.hup);
-      printf("\tUNKNOWN : %d\n",count.unknown);
-   }
-
-
    if (count.total==0) {
       if (!quiet) printf("No overflow events generated.\n");
       test_fail(test_string);
    }
 
-   second_count=count.total;
-   close(fd1);
-
-   if (!quiet) printf("Count1: %d  Count2: %d Expected count2: %d\n",
-		      first_count,second_count,10);
-
-   if (second_count!=10) {
-       if (!quiet) fprintf(stderr,"Error, unexpected count\n");
-       test_fail(test_string);     
+   if (count.hup!=0) {
+      if (!quiet) printf("Unexpected POLL_HUP overflow.\n");
+      test_fail(test_string);
    }
+   
+   
+   if (count.in!=1000) {
+      if (!quiet) printf("Error: POLL_IN of %d but %d expected.\n",
+			count.in,1000);      
+      test_fail(test_string);
+   }
+   
 
    test_pass(test_string);
    
