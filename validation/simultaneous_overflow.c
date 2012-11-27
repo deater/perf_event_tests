a90443bbacfe38a4ecfe69fbdf0dbcd6d7fea989
Vince Weaver <vincent.weaver@maine.edu>
2012-11-27 12:11:40 -0500

    Make the simultaneous_overflow case use RT signals.
    Previously we were using SIGIO which can be lost if one
    comes in while we are in the signal handler.

--- validation/simultaneous_overflow.c
+++ validation/simultaneous_overflow.c
@@ -29,6 +29,8 @@
 #include "perf_helpers.h"
 #include "matrix_multiply.h"
 
+#define MMAP_PAGES 16
+
 #define NUM_EVENTS 2
 
 struct {
@@ -67,7 +69,7 @@ static void our_handler(int signum,siginfo_t *info, void *uc) {
   int ret,i;
   int fd = info->si_fd;
    
-  ret=ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
+  //  ret=ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
    
   for(i=0;i<NUM_EVENTS;i++) {
      if (events[i].fd==fd) {
@@ -79,12 +81,11 @@ static void our_handler(int signum,siginfo_t *info, void *uc) {
   if (i==NUM_EVENTS) printf("fd %d not found\n",fd);
    
   //printf("fd: %d overflowed\n",fd);
-  ret=ioctl(fd, PERF_EVENT_IOC_REFRESH,1);
+  //ret=ioctl(fd, PERF_EVENT_IOC_REFRESH,1);
 
   (void) ret;
    
 }
-
 
 
 int main(int argc, char** argv) {
@@ -94,7 +95,7 @@ int main(int argc, char** argv) {
    struct perf_event_attr pe;
 
    struct sigaction sa;
-   void *our_mmap;
+   void *our_mmap[NUM_EVENTS];
    char test_string[]="Testing multiple event overflow...";
 
    long long counts[NUM_EVENTS];
@@ -118,7 +119,11 @@ int main(int argc, char** argv) {
    sa.sa_sigaction = our_handler;
    sa.sa_flags = SA_SIGINFO;
 
-   if (sigaction( SIGIO, &sa, NULL) < 0) {
+   /* Use Real Time signal, because it is queued                */
+   /* If we just use SIGIO then we lose signals if one comes in */
+   /* while we were handling a previous overflow.               */
+
+   if (sigaction( SIGRTMIN+2, &sa, NULL) < 0) {
      fprintf(stderr,"Error setting up signal handler\n");
      exit(1);
    }
@@ -148,13 +153,13 @@ int main(int argc, char** argv) {
          exit(1);
       }
    
-      /* on older kernels you need this even if you don't use it */
-      our_mmap=mmap(NULL, (1+8)*4096, 
+      our_mmap[i]=mmap(NULL, (1+MMAP_PAGES)*getpagesize(), 
          PROT_READ|PROT_WRITE, MAP_SHARED, events[i].fd, 0);
    
       fcntl(events[i].fd, F_SETFL, O_RDWR|O_NONBLOCK|O_ASYNC);
-      fcntl(events[i].fd, F_SETSIG, SIGIO);
+      fcntl(events[i].fd, F_SETSIG, SIGRTMIN+2);
       fcntl(events[i].fd, F_SETOWN,getpid());
+      
    
       ioctl(events[i].fd, PERF_EVENT_IOC_RESET, 0);   
 
@@ -228,11 +233,11 @@ int main(int argc, char** argv) {
       }
 
       /* on older kernels you need this even if you don't use it */
-      our_mmap=mmap(NULL, (1+8)*4096, 
+      our_mmap[i]=mmap(NULL, (1+MMAP_PAGES)*getpagesize(), 
            PROT_READ|PROT_WRITE, MAP_SHARED, events[i].fd, 0);
    
       fcntl(events[i].fd, F_SETFL, O_RDWR|O_NONBLOCK|O_ASYNC);
-      fcntl(events[i].fd, F_SETSIG, SIGIO);
+      fcntl(events[i].fd, F_SETSIG, SIGRTMIN+2);
       fcntl(events[i].fd, F_SETOWN,getpid());
    
       ioctl(events[i].fd, PERF_EVENT_IOC_RESET, 0);   
@@ -303,7 +308,7 @@ int main(int argc, char** argv) {
    
    test_pass(test_string);
 
-   (void)our_mmap;
+   //   (void)our_mmap;
    
    return 0;
 }
