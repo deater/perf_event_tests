c1e9be5b303979146f29b5aac369810df179fd87
Vince Weaver <vincent.weaver@maine.edu>
2013-04-29 15:19:10 -0400

    uncore_events: add ivy bridge test

--- validation_privileged/uncore_events.c
+++ validation_privileged/uncore_events.c
@@ -24,12 +24,11 @@ char test_string[]="Testing uncore events...";
 int main(int argc, char **argv) {
    
    int ret,quiet;
-  //int cpu;
    struct perf_event_attr pe;
    int fd1;
    long long counts[2];
 
-   int pmu_type,imc0_type=-1;
+   int pmu_type,uncore_type=-1;
 
    DIR *sys_devices;
    struct dirent *dir;
@@ -37,14 +36,38 @@ int main(int argc, char **argv) {
 
    char filename[BUFSIZ];
    
+   char uncore_box_name[BUFSIZ];
+   char uncore_event_name[BUFSIZ];
+   int uncore_event;
+
+   int processor_type;
+
    quiet=test_quiet();
-   
-   //   cpu=detect_processor();
    
    sys_devices=opendir("/sys/bus/event_source/devices/");
    if (sys_devices==NULL) {
       fprintf(stderr,"Couldn't open /sys/bus_event/source/devices/\n");
       test_skip(test_string);
+   }
+
+   processor_type=detect_processor();
+
+   switch(processor_type) {
+      case PROCESSOR_SANDYBRIDGE_EP:
+           strcpy(uncore_box_name,"uncore_imc_0");
+           strcpy(uncore_event_name,"uncore_imc0::UNC_M_CLOCKTICKS");
+           uncore_event=0xff;
+           break;
+      case PROCESSOR_SANDYBRIDGE:
+      case PROCESSOR_IVYBRIDGE:
+           strcpy(uncore_box_name,"uncore_cbox_0");
+           strcpy(uncore_event_name,"snb_unc_cbo0::UNC_CLOCKTICKS");
+           uncore_event=0xff;
+           break;
+      default:
+	   if (!quiet) fprintf(stderr,"Unsupported processor\n");
+	   test_skip(test_string);
+	   break;
    }
 
    //printf("Looking for PMUs:\n");
@@ -64,25 +87,25 @@ int main(int argc, char **argv) {
      //printf("\tFound %s = %d\n",dir->d_name, pmu_type);
 
      fclose(fff);
-     if (!strncmp(dir->d_name,"uncore_imc_0",13)) imc0_type=pmu_type;
+     if (!strcmp(dir->d_name,uncore_box_name)) uncore_type=pmu_type;
 
    }
 
    closedir(sys_devices);
 
-   if (imc0_type==-1) {
-      if (!quiet) fprintf(stderr,"Could not find IMC0\n");
+   if (uncore_type==-1) {
+      if (!quiet) fprintf(stderr,"Could not find %s\n",uncore_box_name);
       test_skip(test_string);
    }
 
    if (!quiet) {
-      printf("Found imc0 at type %d\n",imc0_type);
+      printf("Found %s at type %d\n",uncore_box_name,uncore_type);
    }
 
    memset(&pe,0,sizeof(struct perf_event_attr));
-   pe.type=imc0_type;
+   pe.type=uncore_type;
    pe.size=sizeof(struct perf_event_attr);
-   pe.config=0xff;
+   pe.config=uncore_event;
    pe.disabled=1;
    pe.exclude_user=0;
    pe.exclude_kernel=0;
@@ -118,7 +141,7 @@ int main(int argc, char **argv) {
    }
    
    if (!quiet) {
-      printf("uncore_imc0:UNC_M_CLOCKTICKS %lld\n",counts[0]);
+      printf("%s %lld\n",uncore_event_name,counts[0]);
    }
    if (counts[0]<1) {
       fprintf(stderr,"Retired instruction count too low\n");
