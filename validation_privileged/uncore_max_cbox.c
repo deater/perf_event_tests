dd14dfa1044e72f273ec578561d626e90ad3b981
Vince Weaver <vincent.weaver@maine.edu>
2013-07-10 15:17:47 -0400

    uncore_max_cbox: do a sleep in addition to matrix-multiply

--- validation_privileged/uncore_max_cbox.c
+++ validation_privileged/uncore_max_cbox.c
@@ -1,7 +1,7 @@
 /* This tests CBOX Uncore Events on SNB and IVB     */
 /* These weren't supported until at least Linxu 3.6 */
 
-/* by Vince Weaver, vincent.weaver@maine.edu        */
+/* by Vince Weaver, vincent.weaver _at_ maine.edu   */
 
 #define _GNU_SOURCE
 
@@ -67,8 +67,12 @@ int main(int argc, char **argv) {
            strcpy(uncore_event_name,"snb_unc_cbo0::UNC_CLOCKTICKS");
            uncore_event=0xff;
       case PROCESSOR_IVYBRIDGE:
-           strcpy(uncore_event_name,"UNC_CBO_CACHE_LOOKUP:STATE_MESI:ANY_FILTER");
-           uncore_event=0x508f34;
+           strcpy(uncore_event_name,"UNC_CBO_CACHE_LOOKUP:STATE_I:ANY_FILTER");
+		// uncore_event=0x508f34; /* STATE_MESI */
+		uncore_event=0x508834; /* STATE_I */
+		//uncore_event=0x508134; /* STATE_M */
+		// uncore_event=0x508434; /* STATE_S */
+		//uncore_event=0x508234; /* STATE_E */
            break;
       default:
 	   if (!quiet) fprintf(stderr,"Unsupported processor\n");
@@ -135,11 +139,17 @@ int main(int argc, char **argv) {
 				pe.size=sizeof(struct perf_event_attr);
 				pe.config=uncore_event;
 				pe.disabled=1;
+
+				/* can't filter like this on uncore */
 				pe.exclude_user=0;
 				pe.exclude_kernel=0;
 				pe.exclude_hv=0;
 
-				fd[num_events]=perf_event_open(&pe,-1,0,-1,0);
+				fd[num_events]=perf_event_open(&pe,
+					-1,/* pid, measure all */
+					0, /* cpu, use cpu 0 */
+					-1,/* group leader */
+					0  /* flags */);
 				if (fd[num_events]<0) {
 					if (!quiet) {
 						fprintf(stderr,"Error opening %llx, %d : %s\n",
@@ -167,7 +177,15 @@ int main(int argc, char **argv) {
 		test_fail(test_string);
 	}
 
-	if (!quiet) printf("Starting events\n");
+	if (!quiet) printf("Running on core %d\n",sched_getcpu());
+
+	/**************/
+	/* TEST SLEEP */
+	/**************/
+
+	if (!quiet) printf("Testing sleep:\n");
+
+	if (!quiet) printf("\tStarting events\n");
 
 	for(i=0;i<num_events;i++) {
 		ioctl(fd[i], PERF_EVENT_IOC_RESET, 0);
@@ -178,11 +196,10 @@ int main(int argc, char **argv) {
 		}
    	}
 
-	printf("\t\tRunning on core %d\n",sched_getcpu());
-	printf("\t\t");
-	naive_matrix_multiply(quiet);
+	printf("\t\tsleep(1);\n");
+	sleep(1);
 
-	if (!quiet) printf("Stopping events\n");
+	if (!quiet) printf("\tStopping events\n");
 
 	for(i=0;i<num_events;i++) {
 		ret=ioctl(fd[i], PERF_EVENT_IOC_DISABLE,0);
@@ -192,7 +209,7 @@ int main(int argc, char **argv) {
 		}
 	}
 
-	if (!quiet) printf("Reading events\n");
+	if (!quiet) printf("\tReading events\n");
 
 	for(i=0;i<num_events;i++) {
 		ret=read(fd[i],&counts[i],sizeof(long long));
@@ -204,7 +221,54 @@ int main(int argc, char **argv) {
 			test_fail(test_string);
 		}
 		if (!quiet) {
-			printf("\tcbox%d %s %lld\n",i,uncore_event_name,counts[i]);
+			printf("\t\tcbox%d %s %lld\n",i,uncore_event_name,counts[i]);
+		}
+
+	}
+
+	/***************************/
+	/* Testing Matrix Multiply */
+	/***************************/
+
+	if (!quiet) printf("Testing Matrix Multiply\n");
+
+	if (!quiet) printf("\tStarting events\n");
+
+	for(i=0;i<num_events;i++) {
+		ioctl(fd[i], PERF_EVENT_IOC_RESET, 0);
+		ret=ioctl(fd[i], PERF_EVENT_IOC_ENABLE,0);
+		if (ret<0) {
+			if (!quiet) fprintf(stderr,"Error enabling events\n");
+			test_fail(test_string);
+		}
+   	}
+
+	printf("\t\t");
+	naive_matrix_multiply(quiet);
+
+	if (!quiet) printf("\tStopping events\n");
+
+	for(i=0;i<num_events;i++) {
+		ret=ioctl(fd[i], PERF_EVENT_IOC_DISABLE,0);
+		if (ret<0) {
+			if (!quiet) fprintf(stderr,"Error disabling events\n");
+			test_fail(test_string);
+		}
+	}
+
+	if (!quiet) printf("\tReading events\n");
+
+	for(i=0;i<num_events;i++) {
+		ret=read(fd[i],&counts[i],sizeof(long long));
+		if (ret!=sizeof(long long)) {
+			if (!quiet) {
+				fprintf(stderr,
+					"Unepxected result from read fd[%d]: %d\n",i,ret);
+			}
+			test_fail(test_string);
+		}
+		if (!quiet) {
+			printf("\t\tcbox%d %s %lld\n",i,uncore_event_name,counts[i]);
 		}
 
 	}
