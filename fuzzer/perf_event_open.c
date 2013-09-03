1c4a2e5ae5f1b11860bba11ffef0122de7e9c7f7
Vince Weaver <vincent.weaver@maine.edu>
2013-09-03 16:43:33 -0400

    fuzzer: merge in updated trinity support
    also update the main perf_event.h header file

--- fuzzer/perf_event_open.c
+++ fuzzer/perf_event_open.c
@@ -921,6 +921,14 @@ static long long random_branch_sample_type(void)
 	if (rand_bool())
 		branch_sample |= PERF_SAMPLE_BRANCH_IND_CALL;
 
+	/* Transactional Memory Types */
+	if (rand_bool())
+		branch_sample |= PERF_SAMPLE_BRANCH_ABORT_TX;
+	if (rand_bool())
+		branch_sample |= PERF_SAMPLE_BRANCH_IN_TX;
+	if (rand_bool())
+		branch_sample |= PERF_SAMPLE_BRANCH_NO_TX;
+
 	return branch_sample;
 }
 
@@ -1116,7 +1124,7 @@ static void create_random_event(struct perf_event_attr *attr)
 
 }
 
-static void sanitise_perf_event_open(int childno)
+void sanitise_perf_event_open(int childno)
 {
 	struct perf_event_attr *attr;
 	unsigned long flags;
