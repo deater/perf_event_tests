d0dd239ded9038e581fbe7479ff4b146114953d7
Vince Weaver <vincent.weaver@maine.edu>
2013-12-28 21:03:13 -0500

    tests: check_schedulability -- skip if no perf_event support

--- tests/bugs/check_schedulability.c
+++ tests/bugs/check_schedulability.c
@@ -1,5 +1,5 @@
 /* check_schedulability.c  */
-/* by Vince Weaver   vweaver1 _at_ eecs.utk.edu */
+/* by Vince Weaver -- vincent.weaver @ maine.eduu */
 
 #define _GNU_SOURCE 1
 
@@ -25,82 +25,94 @@
 #include "perf_helpers.h"
 #include "matrix_multiply.h"
 
+#define NUM_EVENTS 7
+
 int main(int argc, char** argv) {
-   
-  int ret,quiet,i,result;
 
-   struct perf_event_attr pe;
+	int ret,quiet,i,result;
 
-   char test_string[]="Testing if events checked for schedulability at read...";
+	struct perf_event_attr pe;
 
-   #define NUM_EVENTS 7
-   int fd[NUM_EVENTS];
-   long long events[NUM_EVENTS]={
-      PERF_COUNT_HW_CPU_CYCLES,
-      PERF_COUNT_HW_INSTRUCTIONS,
-      PERF_COUNT_HW_CACHE_REFERENCES,
-      PERF_COUNT_HW_CACHE_MISSES,
-      PERF_COUNT_HW_BRANCH_MISSES,
-      PERF_COUNT_HW_BRANCH_INSTRUCTIONS,
-      PERF_COUNT_HW_BUS_CYCLES,
-   };
-   
-   quiet=test_quiet();
-   
-   if (!quiet) {
-      printf("Before 2.6.33 events weren't checked to see if they could\n");
-      printf("  run together until read time, rather than at open time.\n");
-   }
+	char test_string[]="Testing if events checked for schedulability at read...";
 
-   fd[0]=-1;
-   for(i=0;i<NUM_EVENTS;i++) {
+	int fd[NUM_EVENTS];
+	long long events[NUM_EVENTS]={
+		PERF_COUNT_HW_CPU_CYCLES,
+		PERF_COUNT_HW_INSTRUCTIONS,
+		PERF_COUNT_HW_CACHE_REFERENCES,
+		PERF_COUNT_HW_CACHE_MISSES,
+		PERF_COUNT_HW_BRANCH_MISSES,
+		PERF_COUNT_HW_BRANCH_INSTRUCTIONS,
+		PERF_COUNT_HW_BUS_CYCLES,
+	};
 
-      memset(&pe,0,sizeof(struct perf_event_attr));
-      pe.type=PERF_TYPE_HARDWARE;
-      pe.size=sizeof(struct perf_event_attr);
-      pe.config=events[i];
-      if (i==0) {
-	pe.disabled=1;
-	pe.pinned=1; /* must be set */
-      }
-      pe.exclude_kernel=1;
-      pe.exclude_hv=1;
+	long long buffer[4096];
 
-      arch_adjust_domain(&pe,quiet);
+	quiet=test_quiet();
 
-      fd[i]=perf_event_open(&pe,0,-1,fd[0],0);
-      if (fd[i]<0) {
-	if (!quiet) fprintf(stderr,"Stopped at event %d %s\n",i,strerror(errno));
-         test_pass(test_string);
-	 exit(1);
-      }
-   }
-      
-   /* enable counting */
-   ret=ioctl(fd[0], PERF_EVENT_IOC_ENABLE,0);
-   if (ret<0) {
-      if (!quiet) fprintf(stderr,"Events failed at ENABLE rather than at open\n");
-      test_fail(test_string);
-   }
-   
-   //   naive_matrix_multiply(quiet);
+	if (!quiet) {
+		printf("Before 2.6.33 events weren't checked to see if they could\n");
+		printf("  run together until read time, rather than at open time.\n");
+	}
 
-   /* disable counting */
-   ret=ioctl(fd[0], PERF_EVENT_IOC_DISABLE,0);
-   if (ret<0) {   
-      if (!quiet) printf("Error disabling\n");
-   }
-     
-   long long buffer[4096];
+	fd[0]=-1;
 
-   result=read(fd[0],buffer,4096);
-   if (result<=0) {
-     if (!quiet) fprintf(stderr,"Test failed at read time\n");
-     test_fail(test_string);
-   }
+	for(i=0;i<NUM_EVENTS;i++) {
 
-   test_pass(test_string);
-      
-   return 0;
+		memset(&pe,0,sizeof(struct perf_event_attr));
+
+		pe.type=PERF_TYPE_HARDWARE;
+		pe.size=sizeof(struct perf_event_attr);
+		pe.config=events[i];
+		if (i==0) {
+			pe.disabled=1;
+			pe.pinned=1; /* must be set */
+		}
+		pe.exclude_kernel=1;
+		pe.exclude_hv=1;
+
+		arch_adjust_domain(&pe,quiet);
+
+		fd[i]=perf_event_open(&pe,0,-1,fd[0],0);
+		if (fd[i]<0) {
+			if (!quiet) {
+				fprintf(stderr,"Stopped at event %d %s\n",i,strerror(errno));
+			}
+
+			if (i==0) {
+				if (!quiet) fprintf(stderr,"Couldn't add events\n");
+				test_skip(test_string);
+			}
+			test_pass(test_string);
+			exit(1);
+		}
+	}
+
+	/* enable counting */
+	ret=ioctl(fd[0], PERF_EVENT_IOC_ENABLE,0);
+	if (ret<0) {
+		if (!quiet) {
+			fprintf(stderr,"Events failed at ENABLE rather than at open\n");
+			test_fail(test_string);
+		}
+	}
+
+	//   naive_matrix_multiply(quiet);
+
+	/* disable counting */
+	ret=ioctl(fd[0], PERF_EVENT_IOC_DISABLE,0);
+	if (ret<0) {
+		if (!quiet) printf("Error disabling\n");
+	}
+
+	result=read(fd[0],buffer,4096);
+	if (result<=0) {
+		if (!quiet) fprintf(stderr,"Test failed at read time\n");
+		test_fail(test_string);
+	}
+
+	test_pass(test_string);
+
+	return 0;
 }
 
