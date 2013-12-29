58e07ee7adfd2d46c5b3437f65d385d18afed8dc
Vince Weaver <vincent.weaver@maine.edu>
2013-12-28 21:08:40 -0500

    tests: check_papi_multiplexing -- skip if no perf_event support

--- tests/multiplexing/check_papi_multiplexing.c
+++ tests/multiplexing/check_papi_multiplexing.c
@@ -1,5 +1,5 @@
 /* check_papi_multiplexing.c  */
-/* by Vince Weaver   vweaver1 _at_ eecs.utk.edu */
+/* by Vince Weaver   vincent.weaver _at_ maine.edu */
 
 /* The PAPI multiplexing code partitions events that don't fit in */
 /*  the HW counters.  This requires failure on open, as checked   */
@@ -31,103 +31,112 @@
 #include "perf_helpers.h"
 #include "matrix_multiply.h"
 
+#define NUM_EVENTS 14
+
 int main(int argc, char** argv) {
-   
-  int ret,quiet,i,result;
 
-   struct perf_event_attr pe;
+	int ret,quiet,i,result;
 
-   char test_string[]="Testing if schedulability checked with pinned=0...";
+	struct perf_event_attr pe;
 
-   #define NUM_EVENTS 14
-   int fd[NUM_EVENTS];
-   long long events[NUM_EVENTS]={
-      PERF_COUNT_HW_CPU_CYCLES,
-      PERF_COUNT_HW_INSTRUCTIONS,
-      PERF_COUNT_HW_CACHE_REFERENCES,
-      PERF_COUNT_HW_CACHE_MISSES,
-      PERF_COUNT_HW_BRANCH_MISSES,
-      PERF_COUNT_HW_BRANCH_INSTRUCTIONS,
-      PERF_COUNT_HW_BUS_CYCLES,
-      PERF_COUNT_HW_CPU_CYCLES,
-      PERF_COUNT_HW_INSTRUCTIONS,
-      PERF_COUNT_HW_CACHE_REFERENCES,
-      PERF_COUNT_HW_CACHE_MISSES,
-      PERF_COUNT_HW_BRANCH_MISSES,
-      PERF_COUNT_HW_BRANCH_INSTRUCTIONS,
-      PERF_COUNT_HW_BUS_CYCLES,
-   };
-   
-   quiet=test_quiet();
-   
-   if (!quiet) {
-      printf("Before 2.6.33 events weren't checked to see if they could\n");
-      printf("  run together until read time, rather than at open time.\n");
-      printf("This broke PAPI multiplexing which depends on knowing at open.\n");
-      printf("  in order to set up partitioning.  This is a different error\n");
-      printf("  case than check_schedulability as the group leader isn't pinned.\n");
-   }
+	char test_string[]="Testing if schedulability checked with pinned=0...";
 
-   fd[0]=-1;
-   for(i=0;i<NUM_EVENTS;i++) {
+	int fd[NUM_EVENTS];
 
-      memset(&pe,0,sizeof(struct perf_event_attr));
-      pe.type=PERF_TYPE_HARDWARE;
-      pe.size=sizeof(struct perf_event_attr);
-      pe.config=events[i];
-      if (i==0) {
-	pe.disabled=1;
-      }
-      pe.exclude_kernel=1;
-      pe.exclude_hv=1;
+	long long events[NUM_EVENTS]={
+		PERF_COUNT_HW_CPU_CYCLES,
+		PERF_COUNT_HW_INSTRUCTIONS,
+		PERF_COUNT_HW_CACHE_REFERENCES,
+		PERF_COUNT_HW_CACHE_MISSES,
+		PERF_COUNT_HW_BRANCH_MISSES,
+		PERF_COUNT_HW_BRANCH_INSTRUCTIONS,
+		PERF_COUNT_HW_BUS_CYCLES,
+		PERF_COUNT_HW_CPU_CYCLES,
+		PERF_COUNT_HW_INSTRUCTIONS,
+		PERF_COUNT_HW_CACHE_REFERENCES,
+		PERF_COUNT_HW_CACHE_MISSES,
+		PERF_COUNT_HW_BRANCH_MISSES,
+		PERF_COUNT_HW_BRANCH_INSTRUCTIONS,
+		PERF_COUNT_HW_BUS_CYCLES,
+	};
 
-      arch_adjust_domain(&pe,quiet);
+	quiet=test_quiet();
 
-      fd[i]=perf_event_open(&pe,0,-1,fd[0],0);
-      if (fd[i]<0) {
-	if (!quiet) fprintf(stderr,"Stopped at event %d %s\n",i,strerror(errno));
-         test_pass(test_string);
-	 exit(1);
-      }
-   }
-      
-   /* enable counting */
-   ret=ioctl(fd[0], PERF_EVENT_IOC_ENABLE,0);
-   if (ret<0) {
-      if (!quiet) fprintf(stderr,"Events failed at ENABLE rather than at open\n");
-      test_kernel_fail(test_string);
-   }
-   
-   //   naive_matrix_multiply(quiet);
+	if (!quiet) {
+		printf("Before 2.6.33 events weren't checked to see if they could\n");
+		printf("  run together until read time, rather than at open time.\n");
+		printf("This broke PAPI multiplexing which depends on knowing at open.\n");
+		printf("  in order to set up partitioning.  This is a different error\n");
+		printf("  case than check_schedulability as the group leader isn't pinned.\n");
+	}
 
-   /* disable counting */
-   ret=ioctl(fd[0], PERF_EVENT_IOC_DISABLE,0);
-   if (ret<0) {   
-      if (!quiet) printf("Error disabling\n");
-   }
-     
-   long long buffer;
-   int zeros=0;
+	fd[0]=-1;
 
-   for(i=0;i<NUM_EVENTS;i++) {
+	for(i=0;i<NUM_EVENTS;i++) {
 
-     result=read(fd[i],&buffer,sizeof(long long));
-      if (result<=0) {
-         if (!quiet) fprintf(stderr,"Test failed at read time\n");
-         test_fail(test_string);
-      }
-      
-      if (buffer==0) zeros++;
-   }
+		memset(&pe,0,sizeof(struct perf_event_attr));
+		pe.type=PERF_TYPE_HARDWARE;
+		pe.size=sizeof(struct perf_event_attr);
+		pe.config=events[i];
+		if (i==0) {
+			pe.disabled=1;
+		}
+		pe.exclude_kernel=1;
+		pe.exclude_hv=1;
 
-   if (!quiet) fprintf(stderr,"%d counters were zero\n",zeros);
+		arch_adjust_domain(&pe,quiet);
 
-   if (zeros==NUM_EVENTS) {
-     if (!quiet) fprintf(stderr,"All counters were zero.\n");
-     test_fail(test_string);
-   }
+		fd[i]=perf_event_open(&pe,0,-1,fd[0],0);
+		if (fd[i]<0) {
+			if (!quiet) {
+				fprintf(stderr,"Stopped at event %d %s\n",i,strerror(errno));
+			}
+			if (i==0) {
+				if (!quiet) printf("No events added\n");
+				test_skip(test_string);
+			}
+			test_pass(test_string);
+			exit(1);
+		}
+	}
 
-   test_pass(test_string);
-      
-   return 0;
+	/* enable counting */
+	ret=ioctl(fd[0], PERF_EVENT_IOC_ENABLE,0);
+	if (ret<0) {
+		if (!quiet) fprintf(stderr,"Events failed at ENABLE rather than at open\n");
+		test_kernel_fail(test_string);
+	}
+
+	//   naive_matrix_multiply(quiet);
+
+	/* disable counting */
+	ret=ioctl(fd[0], PERF_EVENT_IOC_DISABLE,0);
+	if (ret<0) {
+		if (!quiet) printf("Error disabling\n");
+	}
+
+	long long buffer;
+	int zeros=0;
+
+	for(i=0;i<NUM_EVENTS;i++) {
+
+		result=read(fd[i],&buffer,sizeof(long long));
+		if (result<=0) {
+			if (!quiet) fprintf(stderr,"Test failed at read time\n");
+			test_fail(test_string);
+		}
+
+		if (buffer==0) zeros++;
+	}
+
+	if (!quiet) fprintf(stderr,"%d counters were zero\n",zeros);
+
+	if (zeros==NUM_EVENTS) {
+		if (!quiet) fprintf(stderr,"All counters were zero.\n");
+		test_fail(test_string);
+	}
+
+	test_pass(test_string);
+
+	return 0;
 }
