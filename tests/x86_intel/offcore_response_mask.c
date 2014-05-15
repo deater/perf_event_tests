4a03357e2697400d179ffec3d9e32097be85b76e
Vince Weaver <vincent.weaver@maine.edu>
2014-05-15 14:51:14 -0400

    tests: offcore_response_mask: add haswell support

--- tests/x86_intel/offcore_response_mask.c
+++ tests/x86_intel/offcore_response_mask.c
@@ -81,6 +81,13 @@ int main(int argc, char **argv) {
 		events[0]=0x5301b7;
 		events[1]=0x4001;
 	}
+	else if (cpu==PROCESSOR_HASWELL) {
+		if (!quiet) printf("Found Haswell!\n");
+		/* try OFFCORE_RESPONSE_0:DMND_DATA_RD:ANY_RESPONSE */
+		/* libpfm4 reports 0x5301b7 0x10001 */
+		events[0]=0x5301b7;
+		events[1]=0x10001;
+	}
 	else {
 		if (!quiet) printf("No known offcore response hardware\n");
 		test_skip(test_string);
