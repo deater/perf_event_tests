8641cf21ef5ece47f6b9e921b8f369083f6b7b96
Vince Weaver <vincent.weaver@maine.edu>
2014-07-11 23:40:50 -0400

    tests: fixed_ctr0 -- actually make it work as described

--- tests/x86_intel/fixed_ctr0.c
+++ tests/x86_intel/fixed_ctr0.c
@@ -3,12 +3,15 @@
 /* Support added in Linux 2.6.X */
 /* Note: bugs where count not same as same counter in general purpose counter */
 
+/* You cannot specify that you want the fixed counter, Linux */
+/* schedules it if available.  If the NMI watchdog is available */
+/* it probably grabs it, making the test somewhat useless */
+
 /* by Vince Weaver, vincent.weaver@maine.edu          */
 
 
-char test_string[]="Testing fixed counter 0 event...";
-int quiet=0;
-int fd;
+static char test_string[]="Testing fixed counter 0 event...";
+static int quiet=0;
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -23,108 +26,78 @@ int fd;
 #include "perf_helpers.h"
 #include "instructions_testcode.h"
 
+#define MAX_OPEN	16
+
 int main(int argc, char **argv) {
 
 	struct perf_event_attr pe;
-	void *mmap_result;
-	int result;
+	int result=0;
+	int fd[MAX_OPEN];
+	int i;
+	int num_counters=0;
+	int errors=0;
+
 	long long values[256];
 
 	quiet=test_quiet();
 
 	if (!quiet) {
-		printf("This test checks the intel fixed counter 0 %d\n",getpid());
+		printf("This test checks the intel fixed counter 0\n");
+		printf("This is a best effort, Linux does not let you\n");
+		printf("specify which counter events are scheduled in\n");
+		printf("and the NMI watchdog ofen steals the fixed one.\n");
+		printf("Anyway, all the values should match.\n\n");
 	}
-
-	sleep(1);
 
 	memset(&pe,0,sizeof(struct perf_event_attr));
 
 	pe.type=PERF_TYPE_HARDWARE;
-	pe.size=80;
+	pe.size=sizeof(struct perf_event_attr);
 	pe.config=PERF_COUNT_HW_INSTRUCTIONS;
-	pe.sample_period=0xc0000000000000bdULL;
-	pe.sample_type=PERF_SAMPLE_TID|PERF_SAMPLE_ID|PERF_SAMPLE_CPU|PERF_SAMPLE_PERIOD|PERF_SAMPLE_STREAM_ID|PERF_SAMPLE_RAW|PERF_SAMPLE_STACK_USER|PERF_SAMPLE_DATA_SRC|PERF_SAMPLE_IDENTIFIER|PERF_SAMPLE_TRANSACTION; /* 3a7c2 */
-        pe.read_format=PERF_FORMAT_TOTAL_TIME_RUNNING|PERF_FORMAT_GROUP; /* a */
+        pe.read_format=PERF_FORMAT_GROUP;
 
-//	pe.exclude_user=1;
         pe.exclude_kernel=1;
         pe.exclude_hv=1;
-        pe.mmap=1;
-        pe.comm=1;
-        pe.enable_on_exec=1;
-        pe.task=1;
-        pe.watermark=1;
-  //      pe.precise_ip=0; /* arbitrary skid */
-       pe.exclude_callchain_kernel=1;
-        pe.exclude_callchain_user=1;
-        pe.wakeup_watermark=254;
-        pe.branch_sample_type=PERF_SAMPLE_BRANCH_USER|PERF_SAMPLE_BRANCH_ANY_CALL|PERF_SAMPLE_BRANCH_IND_CALL|PERF_SAMPLE_BRANCH_ABORT_TX;
-        pe.sample_regs_user=57;
-        pe.sample_stack_user=510;
 
-	pe.disabled=1;
-	pe.pinned=1;
 
-//	fd=perf_event_open(&pe,0,0,-1,PERF_FLAG_FD_OUTPUT);
+	for(i=0;i<MAX_OPEN;i++) {
 
-	fd=perf_event_open(&pe,0,-1,-1,PERF_FLAG_FD_OUTPUT);
-	if (fd<0) {
-		fprintf(stderr,"Error opening leader %llx %s\n",pe.config,strerror(errno));
+		pe.disabled=(i==0);
+		pe.pinned=(i==0);
+		fd[i]=perf_event_open(&pe,0,-1,i==0?-1:fd[0],0);
+		if (fd[i]<0) {
+			fprintf(stderr,"Error opening event %d %s\n",i,strerror(errno));
+			break;
+		}
+	}
+	num_counters=i;
+	if (num_counters<1) {
 		test_fail(test_string);
 	}
 
-	mmap_result=mmap(NULL, 0x2ae6,PROT_READ|PROT_WRITE, MAP_SHARED,fd, 0);
+	ioctl(fd[0],PERF_EVENT_IOC_ENABLE,0);
+	result+=instructions_million();
 
-	ioctl(fd,PERF_EVENT_IOC_ENABLE,0);
+	ioctl(fd[0],PERF_EVENT_IOC_DISABLE,0);
 
-	int i;
-	result=0;
-	for(i=0;i<1000;i++) {
-		result+=instructions_million();
+	read(fd[0],values,128);
+
+	close(fd[0]);
+
+	for(i=0;i<num_counters;i++) {
+		if (!quiet) {
+			printf("%d %lld\n",i,values[i+1]);
+		}
+		if (values[i+1]!=values[1]) {
+			errors++;
+		}
 	}
 
-	ioctl(fd,PERF_EVENT_IOC_DISABLE,0);
-
-	ioctl(fd,PERF_EVENT_IOC_ENABLE,0);
-	result+=instructions_million();
-	ioctl(fd,PERF_EVENT_IOC_DISABLE,0);
-
-	read(fd,values,128);
-
-//	munmap(mmap_result,10982);
-	close(fd);
-
-	printf("result: %d - %lld %lld %lld\n",result,values[0],values[1],values[2]);
-
-	printf("Years: %lld\n",(((((pe.sample_period / 4000000000ULL)/2ULL)/3600ULL)/24ULL)/365ULL));
-
-	{
-		unsigned long long usp;
-		signed long long period;
-		signed long long period_left=0,left=0;
-
-		usp=pe.sample_period;
-		period=(signed long long)usp;
-		printf("%lld %lld %llx\n",usp,period,period);
-
-		if (left<=-period) {
-			printf("Less than\n");
-			left = period;
-			period_left=left;
-//			hwc->last_period = period;
+	if (errors) {
+		if (!quiet) {
+			fprintf(stderr,"Some results don't match!\n");
 		}
-
-		if (left<=0) {
-			printf("Negative\n");
-			left+=period;
-			period_left=left;
-			printf("%lld %llx\n",period_left,period_left);
-			left+=period;
-			period_left=left;
-			printf("%lld %llx\n",period_left,period_left);
-		}
-
+		test_fail(test_string);
 	}
 
 	test_pass(test_string);
