b995d27efd57e50f5792fda22a5346409877a6e8
Vince Weaver <vince@deater.net>
2014-08-07 23:33:34 -0400

    tests: simpls_overflow_sibling -- update coding style

--- tests/overflow/simple_overflow_sibling.c
+++ tests/overflow/simple_overflow_sibling.c
@@ -1,5 +1,5 @@
 /* simple_overflow_sibling.c  */
-/* by Vince Weaver   vweaver1 _at_ eecs.utk.edu */
+/* by Vince Weaver   vincent.weaver _at_ maine.edu */
 
 /* Test overflow of sibling */
 
@@ -35,120 +35,120 @@ static int fd1,fd2;
 
 static void our_handler(int signum,siginfo_t *oh, void *blah) {
 
-  int ret;
+	int ret;
 
-  count++;
+	count++;
 
-  (void) ret;
+	(void) ret;
 }
 
 
 int main(int argc, char** argv) {
-   
-   int ret,quiet,i;
 
-   struct perf_event_attr pe;
+	int ret,quiet,i;
 
-   struct sigaction sa;
-   void *our_mmap;
-   char test_string[]="Testing overflows on sibling...";
-   
-   quiet=test_quiet();
+	struct perf_event_attr pe;
 
-   if (!quiet) printf("This tests that overflows of siblings work.\n");
-   
-   memset(&sa, 0, sizeof(struct sigaction));
-   sa.sa_sigaction = our_handler;
-   sa.sa_flags = SA_SIGINFO;
+	struct sigaction sa;
+	void *our_mmap;
+	char test_string[]="Testing overflows on sibling...";
 
-   if (sigaction( SIGIO, &sa, NULL) < 0) {
-     fprintf(stderr,"Error setting up signal handler\n");
-     exit(1);
-   }
-   
-   memset(&pe,0,sizeof(struct perf_event_attr));
+	quiet=test_quiet();
 
-   pe.type=PERF_TYPE_HARDWARE;
-   pe.size=sizeof(struct perf_event_attr);
-   pe.config=PERF_COUNT_HW_CPU_CYCLES;
-   pe.sample_period=0;
-   pe.sample_type=0;
-   pe.read_format=PERF_FORMAT_GROUP|PERF_FORMAT_ID;
-   pe.disabled=1;
-   pe.pinned=0;
-   pe.exclude_kernel=1;
-   pe.exclude_hv=1;
-   pe.wakeup_events=1;
+	if (!quiet) printf("This tests that overflows of siblings work.\n");
 
-   arch_adjust_domain(&pe,quiet);
+	memset(&sa, 0, sizeof(struct sigaction));
+	sa.sa_sigaction = our_handler;
+	sa.sa_flags = SA_SIGINFO;
 
-   fd1=perf_event_open(&pe,0,-1,-1,0);
-   if (fd1<0) {
-      fprintf(stderr,"Error opening leader %llx\n",pe.config);
-      test_fail(test_string);
-   }
+	if (sigaction( SIGIO, &sa, NULL) < 0) {
+		fprintf(stderr,"Error setting up signal handler\n");
+		exit(1);
+	}
 
-   pe.type=PERF_TYPE_HARDWARE;
-   pe.config=PERF_COUNT_HW_INSTRUCTIONS;
-   pe.sample_period=100000;
-   pe.sample_type=PERF_SAMPLE_IP;
-   pe.read_format=0;
-   pe.disabled=0;
-   pe.pinned=0;
-   pe.exclude_kernel=1;
-   pe.exclude_hv=1;
+	memset(&pe,0,sizeof(struct perf_event_attr));
 
-   arch_adjust_domain(&pe,quiet);
+	pe.type=PERF_TYPE_HARDWARE;
+	pe.size=sizeof(struct perf_event_attr);
+	pe.config=PERF_COUNT_HW_CPU_CYCLES;
+	pe.sample_period=0;
+	pe.sample_type=0;
+	pe.read_format=PERF_FORMAT_GROUP|PERF_FORMAT_ID;
+	pe.disabled=1;
+	pe.pinned=0;
+	pe.exclude_kernel=1;
+	pe.exclude_hv=1;
+	pe.wakeup_events=1;
 
-   fd2=perf_event_open(&pe,0,-1,fd1,0);
-   if (fd2<0) {
-      fprintf(stderr,"Error opening %llx\n",pe.config);
-      test_fail(test_string);
-   }
+	arch_adjust_domain(&pe,quiet);
 
-   /* large enough that threshold not a problem */
-   our_mmap=mmap(NULL, (1+MMAP_PAGES)*getpagesize(), 
-         PROT_READ|PROT_WRITE, MAP_SHARED, fd2, 0);
-   
-   fcntl(fd2, F_SETFL, O_RDWR|O_NONBLOCK|O_ASYNC);
-   fcntl(fd2, F_SETSIG, SIGIO);
-   fcntl(fd2, F_SETOWN,getpid());
-   
-   ioctl(fd1, PERF_EVENT_IOC_RESET, 0);   
-   ioctl(fd2, PERF_EVENT_IOC_RESET, 0);   
+	fd1=perf_event_open(&pe,0,-1,-1,0);
+	if (fd1<0) {
+		fprintf(stderr,"Error opening leader %llx\n",pe.config);
+		test_fail(test_string);
+	}
 
-   ret=ioctl(fd1, PERF_EVENT_IOC_ENABLE,0);
+	pe.type=PERF_TYPE_HARDWARE;
+	pe.config=PERF_COUNT_HW_INSTRUCTIONS;
+	pe.sample_period=100000;
+	pe.sample_type=PERF_SAMPLE_IP;
+	pe.read_format=0;
+	pe.disabled=0;
+	pe.pinned=0;
+	pe.exclude_kernel=1;
+	pe.exclude_hv=1;
 
-   if (ret<0) {
-      if (!quiet) fprintf(stderr,"Error with PERF_EVENT_IOC_ENABLE of group leader: "
-	     "%d %s\n",errno,strerror(errno));
-      test_fail(test_string);
-   }
+	arch_adjust_domain(&pe,quiet);
 
-   for(i=0;i<10;i++) {
-      instructions_million();
-   }
-   
-   ret=ioctl(fd1, PERF_EVENT_IOC_DISABLE,0);
-    
-   if (!quiet) printf("Count: %d %p\n",count,our_mmap);
+	fd2=perf_event_open(&pe,0,-1,fd1,0);
+	if (fd2<0) {
+		fprintf(stderr,"Error opening %llx\n",pe.config);
+			test_fail(test_string);
+	}
 
-   if (count==0) {
-      if (!quiet) printf("No overflow events generated.\n");
-      test_fail(test_string);
-   }
-   
-   if (count!=100) {
-      if (!quiet) printf("Expected %d overflows, got %d.\n",
-			count,100);
-      test_fail(test_string);
-   }   
+	/* large enough that threshold not a problem */
+	our_mmap=mmap(NULL, (1+MMAP_PAGES)*getpagesize(),
+		PROT_READ|PROT_WRITE, MAP_SHARED, fd2, 0);
 
-   close(fd1);
-   close(fd2);
+	fcntl(fd2, F_SETFL, O_RDWR|O_NONBLOCK|O_ASYNC);
+	fcntl(fd2, F_SETSIG, SIGIO);
+	fcntl(fd2, F_SETOWN,getpid());
 
-   test_pass(test_string);
-   
-   return 0;
+	ioctl(fd1, PERF_EVENT_IOC_RESET, 0);
+	ioctl(fd2, PERF_EVENT_IOC_RESET, 0);
+
+	ret=ioctl(fd1, PERF_EVENT_IOC_ENABLE,0);
+
+	if (ret<0) {
+		if (!quiet) fprintf(stderr,"Error with PERF_EVENT_IOC_ENABLE of group leader: "
+			"%d %s\n",errno,strerror(errno));
+		test_fail(test_string);
+	}
+
+	for(i=0;i<10;i++) {
+		instructions_million();
+	}
+
+	ret=ioctl(fd1, PERF_EVENT_IOC_DISABLE,0);
+
+	if (!quiet) printf("Count: %d %p\n",count,our_mmap);
+
+	if (count==0) {
+		if (!quiet) printf("No overflow events generated.\n");
+		test_fail(test_string);
+	}
+
+	if (count!=100) {
+		if (!quiet) printf("Expected %d overflows, got %d.\n",
+				count,100);
+		test_fail(test_string);
+	}
+
+	close(fd1);
+	close(fd2);
+
+	test_pass(test_string);
+
+	return 0;
 }
 
