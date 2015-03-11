3c2ee576c9c38d423a990f446b4733bfbf4d330d
Vince Weaver <vincent.weaver@maine.edu>
2015-03-11 11:13:56 -0400

    fuzzer: add ARM suport to get_cpuinfo code.

--- fuzzer/get_cpuinfo.c
+++ fuzzer/get_cpuinfo.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <string.h>
+#include <sys/utsname.h>
 
-/* FIXME -- this is x86 specific for now */
-void get_cpuinfo(char *cpuinfo) {
+void get_cpuinfo_x86(char *cpuinfo) {
 
 	FILE *fff;
 	char temp_string[BUFSIZ],temp_string2[BUFSIZ];
@@ -50,6 +51,64 @@ void get_cpuinfo(char *cpuinfo) {
 	fclose(fff);
 
 	sprintf(cpuinfo,"%s %d/%d/%d\n",vendor,family,model,stepping);
+
+}
+
+static void get_cpuinfo_arm(char *cpuinfo) {
+
+	FILE *fff;
+	char *result;
+	char temp_string[BUFSIZ];
+	int architecture,implementer,variant,part,revision;
+
+	fff=fopen("/proc/cpuinfo","r");
+	if (fff==NULL) {
+		strcpy(cpuinfo,"UNKNOWN");
+		return;
+	}
+
+	while(1) {
+		result=fgets(temp_string,BUFSIZ,fff);
+		if (result==NULL) break;
+
+		if (!strncmp(temp_string,"CPU implementer",15)) {
+			sscanf(temp_string,"%*s%*s%*s %x",&implementer);
+		}
+		if (!strncmp(temp_string,"CPU architecture",16)) {
+			sscanf(temp_string,"%*s%*s %x",&architecture);
+		}
+		if (!strncmp(temp_string,"CPU variant",11)) {
+			sscanf(temp_string,"%*s%*s%*s %x",&variant);
+		}
+		if (!strncmp(temp_string,"CPU revision",12)) {
+			sscanf(temp_string,"%*s%*s%*s %x",&revision);
+		}
+		if (!strncmp(temp_string,"CPU part",8)) {
+			sscanf(temp_string,"%*s%*s%*s %x",&part);
+		}
+
+	}
+	fclose(fff);
+
+	sprintf(cpuinfo,"ARMv%d %x %x %x %x\n",
+		architecture,implementer,variant,part,revision);
+
+}
+
+void get_cpuinfo(char *cpuinfo) {
+
+	struct utsname uname_buf;
+
+	uname(&uname_buf);
+
+	if (strstr(uname_buf.machine,"86")) {
+		return get_cpuinfo_x86(cpuinfo);
+	}
+	if (strstr(uname_buf.machine,"arm")) {
+		return get_cpuinfo_arm(cpuinfo);
+	}
+
+	sprintf(cpuinfo,"%s UNKNOWN\n",uname_buf.machine);
 
 }
 
