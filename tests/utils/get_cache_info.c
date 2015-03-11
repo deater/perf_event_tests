7a4544a96126e1069725805149a95db90df9d12e
Vince Weaver <vincent.weaver@maine.edu>
2015-03-11 12:48:06 -0400

    tests: get_cache_info, whitespace changes

--- tests/utils/get_cache_info.c
+++ tests/utils/get_cache_info.c
@@ -4,27 +4,27 @@
 #include "detect_cache.h"
 
 int main(int argc, char **argv) {
-   
-   int quiet;
-   int i,j,max_level=0;
-   char test_string[]="Seeing if cache info is provided by the kernel...";
-   
-   quiet=test_quiet();
- 
-   gather_cache_info(quiet,test_string);
 
-   max_level=cache_get_max_levels(quiet,test_string);
-   if (!quiet) {
-      printf("Max level of cache=%d\n",max_level);
-   }
-   
-   for(i=1;i<=max_level;i++) {
-     for(j=0;j<MAX_CACHE_TYPE;j++) {
-       print_cache_info(quiet,&cache_info[i][j]);
-     }
-   }
+	int quiet;
+	int i,j,max_level=0;
+	char test_string[]="Seeing if cache info is provided by the kernel...";
 
-   test_pass(test_string);
-   
-   return 0;
+	quiet=test_quiet();
+
+	gather_cache_info(quiet,test_string);
+
+	max_level=cache_get_max_levels(quiet,test_string);
+	if (!quiet) {
+		printf("Max level of cache=%d\n",max_level);
+	}
+
+	for(i=1;i<=max_level;i++) {
+		for(j=0;j<MAX_CACHE_TYPE;j++) {
+			print_cache_info(quiet,&cache_info[i][j]);
+		}
+	}
+
+	test_pass(test_string);
+
+	return 0;
 }
