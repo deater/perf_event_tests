55971123723230eebe5cad8f93b53c5e751ffe7d
Vince Weaver <vincent.weaver@maine.edu>
2015-05-19 11:14:54 -0400

    fuzzer: missed include

--- fuzzer/perf_fuzzer.h
+++ fuzzer/perf_fuzzer.h
@@ -1,1 +1,23 @@
+#define NUM_EVENTS 100000
+
+struct event_data_t {
+	int active;
+	int fd;
+	struct perf_event_attr attr;
+	pid_t pid;
+	int cpu;
+	int group_fd;
+	unsigned long flags;
+	int read_size;
+	int number_in_group;
+	struct sigaction sa;
+	char *mmap;
+	int mmap_size;
+	int last_refresh;
+	int overflows;
+	int throttles;
+};
+
+extern struct event_data_t event_data[NUM_EVENTS];
+
 int find_random_active_event(void);
