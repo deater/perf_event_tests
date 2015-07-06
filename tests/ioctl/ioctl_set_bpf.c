2167ada06e0275d7fff6a7bce2dcac8f40dbb755
Vince Weaver <vincent.weaver@maine.edu>
2015-07-06 16:44:28 -0400

    tests: ioctl_set_bpf -- finally managed to create valid event.

--- tests/ioctl/ioctl_set_bpf.c
+++ tests/ioctl/ioctl_set_bpf.c
@@ -31,6 +31,12 @@
 
 #include "bpf.h"
 
+#include "libbpf.h"
+
+#include <linux/version.h>
+
+#include <sys/utsname.h>
+
 //#ifndef __bpf
 //#if defined(__i386__)
 //#define __NR_perf_event_open    336
@@ -73,21 +79,41 @@ int main(int argc, char** argv) {
 		printf("Testing PERF_EVENT_IOC_SET_BPF ioctl.\n");
 	}
 
-	/************************************/
-	/* Creating a tracepoint event      */
-	/************************************/
+	/*******************************************/
+	/* Creating a kprobe tracepoint event      */
+	/*******************************************/
 
 	if (!quiet) {
-		printf("Creating a tracepoint event\n");
+		printf("Creating a kprobe tracepoint event\n");
 	}
+
+	FILE *fff;
+	int kprobe_id;
+
+	fff=fopen("/sys/kernel/tracing/kprobe_events", "w");
+	if (fff==NULL) {
+		printf("Cannot open!\n");
+		test_fail(test_string);
+	}
+
+	fprintf(fff,"p:probe/VMW _text+1664816");
+	fclose(fff);
+
+	fff=fopen("/sys/kernel/tracing/events/probe/VMW/id","r");
+	if (fff==NULL) {
+		printf("Cannot open id!\n");
+		test_fail(test_string);
+	}
+
+	fscanf(fff,"%d",&kprobe_id);
+
+
 
 	memset(&pe1,0,sizeof(struct perf_event_attr));
 	pe1.type=PERF_TYPE_TRACEPOINT;
 	pe1.size=sizeof(struct perf_event_attr);
 
-	/* Try to pick one that is valid on most kernels/machines */
-	/* writeback:writeback_start*/
-	pe1.config=226;
+	pe1.config=kprobe_id;
 
 	pe1.disabled=1;
 	pe1.exclude_kernel=0;
@@ -100,23 +126,48 @@ int main(int argc, char** argv) {
 		if (!quiet) {
 			fprintf(stderr,"Unexpected error %s\n",strerror(errno));
 		}
+		printf("Cannot open kprobe id %d\n",kprobe_id);
 		test_fail(test_string);
 	}
 
-	unsigned char instructions[8192];
+	struct bpf_insn instructions[] = {
+		BPF_MOV64_IMM(BPF_REG_0, 0),		/* r0 = 0 */
+		BPF_EXIT_INSN(),			/* return r0 */
+	};
+
 	unsigned char license[]="GPL";
 
 #define LOG_BUF_SIZE 65536
 	static char bpf_log_buf[LOG_BUF_SIZE];
 
+	/* Kernel will EINVAL if unused bits aren't zero */
+	memset(&battr,0,sizeof(battr));
+
+	/* Version has to match currently running kernel */
+
+	struct utsname version;
+	int major, minor, subminor, version_code;
+
+	uname(&version);
+
+	printf("We are running release %s\n",version.release);
+
+
+	sscanf(version.release,"%d.%d.%d",&major,&minor,&subminor);
+
+	version_code = (major<<16) | (minor<<8) | subminor;
+	printf("Using LINUX_VERSION_CODE: %d\n",
+		version_code);
+
 	battr.prog_type = BPF_PROG_TYPE_KPROBE;
-	battr.insn_cnt = 0;
+//	battr.insn_cnt = sizeof(instructions);
+	battr.insn_cnt= sizeof(instructions) / sizeof(struct bpf_insn);
 	battr.insns = (uint64_t) (unsigned long) instructions;
 	battr.license = (uint64_t) (unsigned long) license;
 	battr.log_buf = (uint64_t) (unsigned long) bpf_log_buf;
 	battr.log_size = LOG_BUF_SIZE;
 	battr.log_level = 1;
-	battr.kern_version = 0;
+	battr.kern_version = version_code;
 
 	bpf_log_buf[0] = 0;
 
