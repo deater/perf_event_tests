d47455c16e69b5dac60e69cbb4732a7f9a2f871e
Vince Weaver <vincent.weaver@maine.edu>
2015-07-08 11:51:48 -0400

    lib: add helper for the bpf syscall
    Common code so we can use it even if the kernel headers aren't up to date.

---
+++ lib/bpf_helpers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <asm/unistd.h>
+
+
+#include "bpf.h"
+
+#ifndef __NR_bpf
+
+#if defined(__i386__)
+#define __NR_bpf		357
+#elif defined(__x86_64__)
+#define __NR_bpf		321
+#elif defined __powerpc__
+#define __NR_bpf		361
+#elif defined __arm__
+#define __NR_bpf		386
+#endif
+
+#endif
+
+
+long sys_bpf(int cmd, union bpf_attr *attr, unsigned long size) {
+
+	return syscall(__NR_bpf, cmd, attr, size);
+}
+
