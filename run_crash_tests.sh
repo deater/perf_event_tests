0193b221cbc74ed7a7033d460aa16bd9e4f521b9
Anders Roxell <anders.roxell@linaro.org>
2015-08-07 22:46:49 +0200

    Makefile: adding install rule
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- run_crash_tests.sh
+++ run_crash_tests.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+cd $(dirname $0)
+PERF_EVENT_ROOT=${PWD}
+
 echo
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "            WARNING               "
@@ -14,27 +17,27 @@ echo "time to change your mind..."
 echo
 sleep 5
 
-./crashes/arm_map_hw_event_oops
-./crashes/arm_validate_event_oops
-./crashes/pe_task_schedule_panic
-./crashes/pe_inherit_memleak
-./crashes/oflo_sw_cpu_clock_crash
-./crashes/sw_event_config_overflow
-./crashes/offcore_response_mask
-./crashes/perf_mmap_close_bug
+${PERF_EVENT_ROOT}/crashes/arm_map_hw_event_oops
+${PERF_EVENT_ROOT}/crashes/arm_validate_event_oops
+${PERF_EVENT_ROOT}/crashes/pe_task_schedule_panic
+${PERF_EVENT_ROOT}/crashes/pe_inherit_memleak
+${PERF_EVENT_ROOT}/crashes/oflo_sw_cpu_clock_crash
+${PERF_EVENT_ROOT}/crashes/sw_event_config_overflow
+${PERF_EVENT_ROOT}/crashes/offcore_response_mask
+${PERF_EVENT_ROOT}/crashes/perf_mmap_close_bug
 # Run twice, sometimes takes two runs
-./crashes/nmi_soft_lockup
-./crashes/nmi_soft_lockup
+${PERF_EVENT_ROOT}/crashes/nmi_soft_lockup
+${PERF_EVENT_ROOT}/crashes/nmi_soft_lockup
 # Run twice, sometimes takes two runs
-./crashes/perf_ftrace_event_crash
-./crashes/perf_ftrace_event_crash
+${PERF_EVENT_ROOT}/crashes/perf_ftrace_event_crash
+${PERF_EVENT_ROOT}/crashes/perf_ftrace_event_crash
 # Run twice, sometimes takes two runs
-./crashes/tracepoint_irq_work_exit
-./crashes/tracepoint_irq_work_exit
-./crashes/css_cgroup_crash
-./crashes/rapl_crash
+${PERF_EVENT_ROOT}/crashes/tracepoint_irq_work_exit
+${PERF_EVENT_ROOT}/crashes/tracepoint_irq_work_exit
+${PERF_EVENT_ROOT}/crashes/css_cgroup_crash
+${PERF_EVENT_ROOT}/crashes/rapl_crash
 
 # Also run the warnings tests
-./warnings/core2_nmi_flood
-./warnings/kec_2122_warn
-./warnings/pec_1076_warn
+${PERF_EVENT_ROOT}/warnings/core2_nmi_flood
+${PERF_EVENT_ROOT}/warnings/kec_2122_warn
+${PERF_EVENT_ROOT}/warnings/pec_1076_warn
