a41dba29bc2189f8cb52fd186b37ea8a08ea8840
Vince Weaver <vincent.weaver@maine.edu>
2015-12-09 12:56:07 -0500

    tests: ioctl_set_filter, search for a working event
    we tried to hard-code one but hardcoded trace event ids are not portable

--- tests/ioctl/ioctl_set_filter.c
+++ tests/ioctl/ioctl_set_filter.c
@@ -42,6 +42,8 @@ static int quiet;
 #define MAX_FILTER 8192
 char filter[MAX_FILTER];
 
+#define MAX_SEARCH	4096
+
 int main(int argc, char** argv) {
 
 	int fd1,i;
@@ -70,10 +72,58 @@ int main(int argc, char** argv) {
 	pe1.type=PERF_TYPE_TRACEPOINT;
 	pe1.size=sizeof(struct perf_event_attr);
 
-	/* Try to pick one that is valid on most kernels/machines */
-	/* writeback:writeback_start*/
-	pe1.config=226;
+	/* Find a trace event that will let us add a particular filter */
 
+	/* It should work with */
+	/* writeback:writeback_start*/
+	/* but we can't get the id of this directly without debugfs/tracefs */
+	/* mounted (the id numbers change depending on machine/kernel)      */
+
+	/* Valid filter */
+	strcpy(filter,"nr_pages==2");
+
+	if (!quiet) {
+		printf("Trying to find an event that will allow filter %s\n",
+			filter);
+	}
+
+	/* Usually there are fewer than 1000 trace events? */
+	for(i=0;i<MAX_SEARCH;i++) {
+
+		pe1.config=i;
+		pe1.disabled=1;
+		pe1.exclude_kernel=0;
+		pe1.exclude_hv=0;
+		arch_adjust_domain(&pe1,quiet);
+
+		fd1=perf_event_open(&pe1,0,-1,-1,0);
+		if (fd1<0) {
+			if (!quiet) {
+				// fprintf(stderr,"Failed on %d\n",i);
+			}
+			continue;
+		}
+
+		result=ioctl(fd1, PERF_EVENT_IOC_SET_FILTER, filter);
+		if (result==0) {
+			if (!quiet) printf("Found proper event %d\n",i);
+			close(fd1);
+			break;
+		}
+		else {
+
+		}
+		close(fd1);
+	}
+
+	if (i==MAX_SEARCH) {
+		if (!quiet) {
+			printf("Could not find any trace event to filter\n");
+		}
+		errors++;
+	}
+
+	pe1.config=i;
 	pe1.disabled=1;
 	pe1.exclude_kernel=0;
 	pe1.exclude_hv=0;
@@ -163,20 +213,7 @@ int main(int argc, char** argv) {
 		errors++;
 	}
 
-	/* Valid filter */
-	if (!quiet) {
-		printf("\t+ Valid filter: ");
-	}
-	strcpy(filter,"nr_pages==2");
 
-	result=ioctl(fd1, PERF_EVENT_IOC_SET_FILTER, filter);
-	if (result==0) {
-		if (!quiet) printf("Success\n");
-	}
-	else {
-		if (!quiet) printf("Unexpected %d %s\n",result,strerror(errno));
-		errors++;
-	}
 
 
 	/* tracefs usually under /sys/kernel/tracing */
