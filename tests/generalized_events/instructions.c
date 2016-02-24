d06de23071c75ab3aeb0292dfe9af5079fad36a0
Vince Weaver <vincent.weaver@maine.edu>
2016-02-24 18:33:23 +0000

    tests: instructions, cleanup whitespace

--- tests/generalized_events/instructions.c
+++ tests/generalized_events/instructions.c
@@ -1,9 +1,9 @@
-/* This file attempts to test the retired instruction */
-/* performance counter on various architectures, as   */
-/* implemented by the perf_event generalized event    */ 
-/*      instructions                                  */
+/* This file attempts to test the retired instruction	*/
+/* performance counter on various architectures, as	*/
+/* implemented by the perf_event generalized event	*/
+/*      instructions					*/
 
-/* by Vince Weaver, vweaver1 _at_ eecs.utk.edu        */
+/* by Vince Weaver, vincent.weaver _at_ maine.edu	*/
 
 
 char test_string[]="Testing \"instructions\" generalized event...";
@@ -32,200 +32,202 @@ int fd;
 
 void test_million(void) {
 
-   int i,result,read_result;
+	int i,result,read_result;
 
-   long long count,high=0,low=0,total=0,average=0;
-   double error;
+	long long count,high=0,low=0,total=0,average=0;
+	double error;
 
-   if (!quiet) {
-      printf("\nTesting a loop of 1 million instructions (%d times):\n",
-          NUM_RUNS);
-   }
+	if (!quiet) {
+		printf("\nTesting a loop of 1 million instructions (%d times):\n",
+		NUM_RUNS);
+	}
 
-   for(i=0;i<NUM_RUNS;i++) {
+	for(i=0;i<NUM_RUNS;i++) {
 
-     ioctl(fd, PERF_EVENT_IOC_RESET, 0);
-     ioctl(fd, PERF_EVENT_IOC_ENABLE,0);
+		ioctl(fd, PERF_EVENT_IOC_RESET, 0);
+		ioctl(fd, PERF_EVENT_IOC_ENABLE,0);
 
-     result=instructions_million();
+		result=instructions_million();
 
-     ioctl(fd, PERF_EVENT_IOC_DISABLE,0);
-     read_result=read(fd,&count,sizeof(long long));
+		ioctl(fd, PERF_EVENT_IOC_DISABLE,0);
+		read_result=read(fd,&count,sizeof(long long));
 
-     if (result==CODE_UNIMPLEMENTED) {
-	fprintf(stderr,"\tCode unimplemented\n");
-	test_fail(test_string);	
-     }
+		if (result==CODE_UNIMPLEMENTED) {
+			fprintf(stderr,"\tCode unimplemented\n");
+			test_fail(test_string);
+		}
 
-     if (read_result!=sizeof(long long)) {
-	fprintf(stderr,"\tImproper return from read: %d\n",read_result);
-	test_fail(test_string);	
-     }
+		if (read_result!=sizeof(long long)) {
+			fprintf(stderr,"\tImproper return from read: %d\n",read_result);
+			test_fail(test_string);
+		}
 
-     if (count>high) high=count;
-      if ((low==0) || (count<low)) low=count;
-      total+=count;
-   }
+		if (count>high) high=count;
+		if ((low==0) || (count<low)) low=count;
+		total+=count;
+	}
 
-   average=total/NUM_RUNS;
+	average=total/NUM_RUNS;
 
-   error=display_error(average,high,low,1000000ULL,quiet);
+	error=display_error(average,high,low,1000000ULL,quiet);
 
-   if ((error > 1.0) || (error<-1.0)) {
+	if ((error > 1.0) || (error<-1.0)) {
 
 #if defined(__PPC__)
 
-     if(!quiet) printf("If PPC is off by 50%%, this might be due to\n"
-             "\"folded\" branch instructions on PPC32\n");
-
+		if(!quiet) {
+			printf("If PPC is off by 50%%, this might be due to\n"
+				"\"folded\" branch instructions on PPC32\n");
+		}
 #endif
 
-      test_fail(test_string);
-   }
+		test_fail(test_string);
+	}
 }
 
 /* Test fldcw.  Pentium 4 overcounts this instruction */
 
 void test_fldcw(void) {
 
-#if defined(__i386__) || (defined __x86_64__)   
-   int i,result,read_result;
+#if defined(__i386__) || (defined __x86_64__)
+	int i,result,read_result;
 
-   long long count,high=0,low=0,total=0,average=0;
-   double error;
+	long long count,high=0,low=0,total=0,average=0;
+	double error;
 
-   if (!quiet) {
-     printf("\nTesting a fldcw loop of 900,000 instructions (%d times):\n",
-          NUM_RUNS);
-   }
+	if (!quiet) {
+		printf("\nTesting a fldcw loop of 900,000 instructions (%d times):\n",
+		NUM_RUNS);
+	}
 
-   for(i=0;i<NUM_RUNS;i++) {
-     
-      ioctl(fd, PERF_EVENT_IOC_RESET, 0);
-      ioctl(fd, PERF_EVENT_IOC_ENABLE,0);
+	for(i=0;i<NUM_RUNS;i++) {
 
-      result=instructions_fldcw();
+		ioctl(fd, PERF_EVENT_IOC_RESET, 0);
+		ioctl(fd, PERF_EVENT_IOC_ENABLE,0);
 
-      ioctl(fd, PERF_EVENT_IOC_DISABLE,0);     
-      read_result=read(fd,&count,sizeof(long long));
+		result=instructions_fldcw();
 
-      if (result==CODE_UNIMPLEMENTED) {
-	 fprintf(stderr,"\tCode unimplemented\n");
-	 test_fail(test_string);
-      }
-      
-      if (read_result!=sizeof(long long)) {
-	 fprintf(stderr,"\tUnexpected read result: %d\n",read_result);
-	 test_fail(test_string);
+		ioctl(fd, PERF_EVENT_IOC_DISABLE,0);
+		read_result=read(fd,&count,sizeof(long long));
 
-      }
-      
-      if (count>high) high=count;
-      if ((low==0) || (count<low)) low=count;
-      total+=count;
-   }
+		if (result==CODE_UNIMPLEMENTED) {
+			fprintf(stderr,"\tCode unimplemented\n");
+			test_fail(test_string);
+		}
 
-   average=total/NUM_RUNS;
-   
-   error=display_error(average,high,low,900000ULL,quiet);
-   
-   if ((error > 1.0) || (error<-1.0)) {
+		if (read_result!=sizeof(long long)) {
+			fprintf(stderr,"\tUnexpected read result: %d\n",read_result);
+			test_fail(test_string);
+		}
 
-     if (!quiet) {
-        printf("On Pentium 4 machines, the fldcw instruction counts as 2.\n");
-        printf("This will lead to an overcount of 22%%\n");
-     }
-     test_fail( test_string);
-   }
+		if (count>high) high=count;
+		if ((low==0) || (count<low)) low=count;
+		total+=count;
+	}
+
+	average=total/NUM_RUNS;
+
+	error=display_error(average,high,low,900000ULL,quiet);
+
+	if ((error > 1.0) || (error<-1.0)) {
+
+		if (!quiet) {
+			printf("On Pentium 4 machines, the fldcw instruction counts as 2.\n");
+			printf("This will lead to an overcount of 22%%\n");
+		}
+		test_fail( test_string);
+	}
 #endif
-   
 }
 
 /* Test rep-prefixed instructions. */
 /* HW counters count this as one each, not one per repeat */
 
 void test_rep(void) {
-   
-#if defined(__i386__) || (defined __x86_64__)   
-  int i,result,read_result;
 
-   long long count,high=0,low=0,total=0,average=0;
-   double error;
+#if defined(__i386__) || (defined __x86_64__)
+	int i,result,read_result;
 
-   if(!quiet) printf("\nTesting a 16k rep loop (%d times):\n",
-          NUM_RUNS);
+	long long count,high=0,low=0,total=0,average=0;
+	double error;
 
-   for(i=0;i<NUM_RUNS;i++) {
+	if(!quiet) {
+		printf("\nTesting a 16k rep loop (%d times):\n", NUM_RUNS);
+	}
 
-      ioctl(fd, PERF_EVENT_IOC_RESET, 0);
-      ioctl(fd, PERF_EVENT_IOC_ENABLE,0);
+	for(i=0;i<NUM_RUNS;i++) {
 
-      result=instructions_rep();
-      
-      ioctl(fd, PERF_EVENT_IOC_DISABLE,0);     
-      read_result=read(fd,&count,sizeof(long long));
+		ioctl(fd, PERF_EVENT_IOC_RESET, 0);
+		ioctl(fd, PERF_EVENT_IOC_ENABLE,0);
 
-      if (result==CODE_UNIMPLEMENTED) {
-	 fprintf(stderr,"\tCode unimplemented\n");
-         test_fail(test_string);
-      }
-     
-      if (read_result!=sizeof(long long)) {
-	 fprintf(stderr,"\tUnexpected read result: %d\n",read_result);
-	 test_fail(test_string);
-      }
-      
-      if (count>high) high=count;
-      if ((low==0) || (count<low)) low=count;
-      total+=count;
-   }
+		result=instructions_rep();
 
-   average=total/NUM_RUNS;
-   
-   error=display_error(average,high,low,6002,quiet);
+		ioctl(fd, PERF_EVENT_IOC_DISABLE,0);
+		read_result=read(fd,&count,sizeof(long long));
 
-   if ((error > 10.0) || (error<-10.0)) {
-     if (!quiet) printf("Instruction count off by more than 10%%\n");
+		if (result==CODE_UNIMPLEMENTED) {
+			fprintf(stderr,"\tCode unimplemented\n");
+			test_fail(test_string);
+		}
 
-     test_fail( test_string);
-   }
+		if (read_result!=sizeof(long long)) {
+			fprintf(stderr,"\tUnexpected read result: %d\n",read_result);
+			test_fail(test_string);
+		}
+
+		if (count>high) high=count;
+		if ((low==0) || (count<low)) low=count;
+		total+=count;
+	}
+
+	average=total/NUM_RUNS;
+
+	error=display_error(average,high,low,6002,quiet);
+
+	if ((error > 10.0) || (error<-10.0)) {
+		if (!quiet) {
+			printf("Instruction count off by more than 10%%\n");
+		}
+		test_fail( test_string);
+	}
 #endif
-
 }
 
 
 int main(int argc, char **argv) {
 
-   struct perf_event_attr pe;
+	struct perf_event_attr pe;
 
-   quiet=test_quiet();
-   
-   if (!quiet) printf("This test checks that the \"instructions\" generalized "
-		      "event is working.\n");
+	quiet=test_quiet();
 
-   memset(&pe,0,sizeof(struct perf_event_attr));
-   pe.type=PERF_TYPE_HARDWARE;
-   pe.size=sizeof(struct perf_event_attr);
-   pe.config=PERF_COUNT_HW_INSTRUCTIONS;
-   pe.disabled=1;
-   pe.exclude_kernel=1;
-   pe.exclude_hv=1;
+	if (!quiet) {
+		printf("This test checks that the \"instructions\" generalized "
+			"event is working.\n");
+	}
 
-   arch_adjust_domain(&pe,quiet);
+	memset(&pe,0,sizeof(struct perf_event_attr));
+	pe.type=PERF_TYPE_HARDWARE;
+	pe.size=sizeof(struct perf_event_attr);
+	pe.config=PERF_COUNT_HW_INSTRUCTIONS;
+	pe.disabled=1;
+	pe.exclude_kernel=1;
+	pe.exclude_hv=1;
 
-   fd=perf_event_open(&pe,0,-1,-1,0);
-   if (fd<0) {
-     fprintf(stderr,"Error opening leader %llx %s\n",pe.config,strerror(errno));
-     test_fail(test_string);
-   }
+	arch_adjust_domain(&pe,quiet);
 
-   test_million();
-   test_fldcw();
-   test_rep();
-   
-   close(fd);
+	fd=perf_event_open(&pe,0,-1,-1,0);
+	if (fd<0) {
+		fprintf(stderr,"Error opening leader %llx %s\n",pe.config,strerror(errno));
+		test_fail(test_string);
+	}
 
-   test_pass(test_string);
-   
-   return 0;
+	test_million();
+	test_fldcw();
+	test_rep();
+
+	close(fd);
+
+	test_pass(test_string);
+
+	return 0;
 }
