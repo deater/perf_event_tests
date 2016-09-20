ec5d022fbb37e8a0622589217bede76dad8d227f
Vince Weaver <vincent.weaver@maine.edu>
2016-09-20 12:38:07 -0400

    fuzzer: update access to also access /proc/sys/kernel/nmi_watchdog
    a recent linux-kernel report showed that the BTS driver would crash
    if it were disabled.
    
    This of course only happens if run as root.
    
    Some other changes too, make read/write calls a bit more random

--- fuzzer/fuzz_access.c
+++ fuzzer/fuzz_access.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 #include <signal.h>
 
@@ -11,24 +13,66 @@
 #include "fuzzer_logging.h"
 #include "fuzzer_stats.h"
 #include "fuzz_access.h"
+#include "fuzzer_random.h"
+
+#define BUFFER_SIZE 9000
+
+static char buffer[BUFFER_SIZE];
+
+#define MAX_FILENAMES	5
+
+static char *filenames[MAX_FILENAMES]={
+	"/proc/sys/kernel/perf_cpu_time_max_percent",
+	"/proc/sys/kernel/perf_event_paranoid",
+	"/proc/sys/kernel/perf_event_max_sample_rate",
+	"/proc/sys/kernel/perf_event_mlock_kb",
+	"/proc/sys/kernel/nmi_watchdog"
+};
+
+static int setup_random_write_value(void) {
+
+	int type=rand()%3;
+	int i,size;
+	char string[BUFSIZ];
+
+	/* single small integer */
+	if (type==0) {
+		switch(rand()%5) {
+			case 0: sprintf(buffer,"%d",-1); break;
+			case 1: sprintf(buffer,"%d",0); break;
+			case 2: sprintf(buffer,"%d",1); break;
+			case 3: sprintf(buffer,"%d",2); break;
+			case 4: sprintf(buffer,"%d",3); break;
+		}
+		size=strlen(buffer);
+	}
+
+	/* random series of integers */
+	if (type==1) {
+		buffer[0]=0;
+		for(i=0;i<rand()%4;i++) {
+			sprintf(string,"%d",rand());
+			strcat(buffer,string);
+		}
+		size=strlen(string);
+	}
+
+	/* completely random */
+	if (type==2) {
+		size=rand()%BUFFER_SIZE;
+		for(i=0;i<size;i++) buffer[i]=rand();
+	}
+
+	return size;
+}
 
 void access_random_file(void) {
 
 	int which_file;
-	FILE *fff;
-	char buffer[2048];
+	int fd;
 	int result;
-	int size;
-	long long write_value;
-
-#define MAX_FILENAMES	4
-
-	char *filenames[MAX_FILENAMES]={
-		"/proc/sys/kernel/perf_cpu_time_max_percent",
-		"/proc/sys/kernel/perf_event_paranoid",
-		"/proc/sys/kernel/perf_event_max_sample_rate",
-		"/proc/sys/kernel/perf_event_mlock_kb",
-	};
+	int read_size,write_size;
+	int which;
 
 	/* These files typically are owned by root */
 	/* So this should never trigger any bugs   */
@@ -37,51 +81,59 @@ void access_random_file(void) {
 	stats.access_attempts++;
 
 	which_file=rand()%MAX_FILENAMES;
+	read_size=rand()%BUFFER_SIZE;
 
 	/* FIXME: move write_value code out here if possible */
 
-	if (rand()%2) {
+	/* Randomly pick read/write/just-open/just-close */
+	which=rand()%4;
+
+	if (which==0) {
 
 		if (ignore_but_dont_skip.access) return;
 
-		/* read */
-		fff=fopen(filenames[which_file],"r");
-		if (fff!=NULL) {
-			size=2048;
-			result=fread(buffer,sizeof(char),size,fff);
+		/* read.  Make flags random too? */
+		fd=open(filenames[which_file],rand_open_flags());
+		if (fd>0) {
+			result=read(fd,buffer,read_size);
 
 			if (logging&TYPE_ACCESS) {
 				sprintf(log_buffer,"A 0 %d %d %d\n",
-					which_file,size,result);
+					which_file,read_size,result);
 				write(log_fd,log_buffer,strlen(log_buffer));
 			}
 
 			if (result>0) stats.access_successful++;
 
-			fclose(fff);
+			close(fd);
 		}
 	}
-	else {
+	else if (which==1) {
 		/* write */
-		fff=fopen(filenames[which_file],"w");
-		if (fff!=NULL) {
-			write_value=((unsigned long long)rand()<<32)|rand();
+		fd=open(filenames[which_file],rand_open_flags());
+		if (fd>0) {
+			if (!ignore_but_dont_skip.access) {
 
+			write_size=setup_random_write_value();
 
-		if (!ignore_but_dont_skip.access) {
-			result=fprintf(fff,"%lld\n",write_value);
+			result=write(fd,buffer,write_size);
 
 			if (logging&TYPE_ACCESS) {
-				sprintf(log_buffer,"A 1 %d %lld %d\n",
-					which_file,write_value,result);
+				sprintf(log_buffer,"A 1 %d %d %d\n",
+					which_file,write_size,result);
 				write(log_fd,log_buffer,strlen(log_buffer));
 			}
 
 			if (result>0) stats.access_successful++;
-		}
+			}
 
-			fclose(fff);
+			close(fd);
 		}
+	} else if (which==2) {
+		/* Leave open a certain number */
+
+	} else {
+		/* close a random open */
 	}
 
 
