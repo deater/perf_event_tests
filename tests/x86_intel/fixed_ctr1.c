214642cae9c0eff83b9076f9f37d81fe8b8da8c2
Vince Weaver <vincent.weaver@maine.edu>
2016-12-12 13:56:53 -0500

    tests: fixed_ctr1 hide error that really wasn't
    also make the test work (well, at least not fail oddly) on
    systems w/o the watchdog timer enabled

--- tests/x86_intel/fixed_ctr1.c
+++ tests/x86_intel/fixed_ctr1.c
@@ -33,21 +33,27 @@ int main(int argc, char **argv) {
 	struct perf_event_attr pe;
 	int result=0;
 	int fd[MAX_OPEN];
-	int i;
+	int i,j;
 	int num_counters=0;
-	int errors=0;
+	int errors=0,total_errors=0;
+	int watchdog_enabled;
 
 	long long values[256];
 
 	quiet=test_quiet();
+	watchdog_enabled=detect_nmi_watchdog();
 
 	if (!quiet) {
 		printf("This test checks the intel fixed counter 1\n");
 		printf("This is a best effort, Linux does not let you\n");
 		printf("specify which counter events are scheduled in.\n");
-		printf("The NMI watchdog often grabs fixed counter 1,\n");
-		printf("hiding the issue.\n");
 		printf("Anyway, all the values should match.\n\n");
+	}
+
+	if (watchdog_enabled) {
+		printf("The NMI watchdog is enabled.\n");
+		printf("This often grabs fixed counter 1,\n");
+		printf("hiding the issue.\n\n");
 	}
 
 	if (detect_vendor()!=VENDOR_INTEL) test_skip(test_string);
@@ -62,44 +68,71 @@ int main(int argc, char **argv) {
         pe.exclude_kernel=1;
         pe.exclude_hv=1;
 
+	for(j=0;j<20;j++) {
+		errors=0;
+		/* Detect max events we can have open */
+		for(i=0;i<MAX_OPEN;i++) {
+			pe.disabled=(i==0);
+			pe.pinned=(i==0);
+			fd[i]=perf_event_open(&pe,0,-1,i==0?-1:fd[0],0);
+			if (fd[i]<0) {
+//				if (!quiet) fprintf(stderr,"Stopped at event#%d: %s\n",i,strerror(errno));
+				break;
+			}
+		}
 
-	for(i=0;i<MAX_OPEN;i++) {
+		num_counters=i;
+		if (num_counters<1) {
+			test_fail(test_string);
+		}
 
-		pe.disabled=(i==0);
-		pe.pinned=(i==0);
-		fd[i]=perf_event_open(&pe,0,-1,i==0?-1:fd[0],0);
-		if (fd[i]<0) {
-			fprintf(stderr,"Error opening event %d %s\n",i,strerror(errno));
-			break;
+		/* Due to foolishness in the perf_event interface */
+		/* The number of counters cannot be properly detected */
+		/* At open time of the watchdog counter is enabled */
+		if (watchdog_enabled) {
+			for(i=0;i<num_counters;i++) {
+				close(fd[i]);
+			}
+			num_counters--;
+			for(i=0;i<num_counters;i++) {
+				pe.disabled=(i==0);
+				pe.pinned=(i==0);
+				fd[i]=perf_event_open(&pe,0,-1,i==0?-1:fd[0],0);
+			}
+
+		}
+
+		ioctl(fd[0],PERF_EVENT_IOC_ENABLE,0);
+
+		result+=instructions_million();
+
+		ioctl(fd[0],PERF_EVENT_IOC_DISABLE,0);
+
+		read(fd[0],values,128);
+
+		for(i=0;i<num_counters;i++) {
+			if (!quiet) {
+				printf("%d %lld, ",i,values[i+1]);
+			}
+			if (values[i+1]!=values[1]) {
+				errors++;
+			}
+		}
+		if (!quiet) printf("\n");
+
+		for(i=0;i<num_counters;i++) {
+			close(fd[i]);
+		}
+
+		if (errors) {
+			if (!quiet) {
+				fprintf(stderr,"Some results don't match!\n");
+			}
+			total_errors++;
 		}
 	}
-	num_counters=i;
-	if (num_counters<1) {
-		test_fail(test_string);
-	}
 
-	ioctl(fd[0],PERF_EVENT_IOC_ENABLE,0);
-	result+=instructions_million();
-
-	ioctl(fd[0],PERF_EVENT_IOC_DISABLE,0);
-
-	read(fd[0],values,128);
-
-	close(fd[0]);
-
-	for(i=0;i<num_counters;i++) {
-		if (!quiet) {
-			printf("%d %lld\n",i,values[i+1]);
-		}
-		if (values[i+1]!=values[1]) {
-			errors++;
-		}
-	}
-
-	if (errors) {
-		if (!quiet) {
-			fprintf(stderr,"Some results don't match!\n");
-		}
+	if (total_errors>0) {
 		test_fail(test_string);
 	}
 
