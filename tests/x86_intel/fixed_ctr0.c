b8b53653462bc5ddfb5ccc4e68a1455f269f55b4
Vince Weaver <vincent.weaver@maine.edu>
2016-12-12 13:38:06 -0500

    tests: fixed_ctr0: clean up, remove confusing error message
    we were reporting an error that wasn't.
    
    also clear up a bit what the test is trying to do

--- tests/x86_intel/fixed_ctr0.c
+++ tests/x86_intel/fixed_ctr0.c
@@ -1,7 +1,12 @@
 /* This tests using the intel Fixed Counter 0 */
 /* Which maps to retired_instructions         */
 /* Support added in Linux 2.6.X */
-/* Note: bugs where count not same as same counter in general purpose counter */
+
+/* Note: the fixed counter0 (retired_instructions) */
+/*	does not always match the value read by the */
+/*	retired_instructions event in a general purpose counter */
+/*	so the fact Linux uses them interchangably can lead */
+/*	to unexpected results */
 
 /* You cannot specify that you want the fixed counter, Linux */
 /* schedules it if available.				     */
@@ -33,9 +38,9 @@ int main(int argc, char **argv) {
 	struct perf_event_attr pe;
 	int result=0;
 	int fd[MAX_OPEN];
-	int i;
+	int i,j;
 	int num_counters=0;
-	int errors=0;
+	int errors=0,total_errors=0;
 
 	long long values[256];
 
@@ -43,9 +48,12 @@ int main(int argc, char **argv) {
 
 	if (!quiet) {
 		printf("This test checks the intel fixed counter 0\n");
-		printf("This is a best effort, Linux does not let you\n");
-		printf("specify which counter events are scheduled in.\n");
-		printf("Anyway, all the values should match.\n\n");
+		printf("Which is retired instructions.\n");
+		printf("Linux treats/schedules this the same as in\n");
+		printf("a general purpose counter even though the results\n");
+		printf("are not always equivelent.\n");
+		printf("There is no way to specify which of the two you want\n\n");
+		printf("Ideally, all the values should match.\n\n");
 	}
 
 	if (detect_vendor()!=VENDOR_INTEL) test_skip(test_string);
@@ -60,44 +68,58 @@ int main(int argc, char **argv) {
         pe.exclude_kernel=1;
         pe.exclude_hv=1;
 
+	for(j=0;j<20;j++) {
 
-	for(i=0;i<MAX_OPEN;i++) {
+		errors=0;
+		for(i=0;i<MAX_OPEN;i++) {
 
-		pe.disabled=(i==0);
-		pe.pinned=(i==0);
-		fd[i]=perf_event_open(&pe,0,-1,i==0?-1:fd[0],0);
-		if (fd[i]<0) {
-			fprintf(stderr,"Error opening event %d %s\n",i,strerror(errno));
-			break;
+			pe.disabled=(i==0);
+			pe.pinned=(i==0);
+			fd[i]=perf_event_open(&pe,0,-1,i==0?-1:fd[0],0);
+			if (fd[i]<0) {
+//				if (!quiet) {
+//					fprintf(stderr,"Stopped at event %d: %s\n",
+//						i,strerror(errno));
+//				}
+				break;
+			}
+		}
+		num_counters=i;
+		if (num_counters<1) {
+			test_fail(test_string);
+		}
+
+		ioctl(fd[0],PERF_EVENT_IOC_ENABLE,0);
+		result+=instructions_million();
+
+		ioctl(fd[0],PERF_EVENT_IOC_DISABLE,0);
+
+		read(fd[0],values,128);
+
+		for(i=0;i<num_counters;i++) {
+			if (!quiet) {
+				printf("%d %lld, ",i,values[i+1]);
+			}
+			if (values[i+1]!=values[1]) {
+				errors++;
+			}
+		}
+		if (!quiet) printf("\n");
+
+		if (errors) {
+			if (!quiet) {
+				fprintf(stderr,"Some results don't match!\n");
+			}
+			total_errors++;
+
+		}
+
+		for(i=0;i<num_counters;i++) {
+			close(fd[i]);
 		}
 	}
-	num_counters=i;
-	if (num_counters<1) {
-		test_fail(test_string);
-	}
 
-	ioctl(fd[0],PERF_EVENT_IOC_ENABLE,0);
-	result+=instructions_million();
-
-	ioctl(fd[0],PERF_EVENT_IOC_DISABLE,0);
-
-	read(fd[0],values,128);
-
-	close(fd[0]);
-
-	for(i=0;i<num_counters;i++) {
-		if (!quiet) {
-			printf("%d %lld\n",i,values[i+1]);
-		}
-		if (values[i+1]!=values[1]) {
-			errors++;
-		}
-	}
-
-	if (errors) {
-		if (!quiet) {
-			fprintf(stderr,"Some results don't match!\n");
-		}
+	if (total_errors>0) {
 		test_fail(test_string);
 	}
 
