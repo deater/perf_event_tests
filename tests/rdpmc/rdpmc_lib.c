f70b835b1ca8b9c1ea728310b73742a8ee175fdf
Vince Weaver <vincent.weaver@maine.edu>
2017-03-06 16:58:52 -0500

    rdpmc: update the mmap_read() code
    went through and made sure I mostly understand what is going on

--- tests/rdpmc/rdpmc_lib.c
+++ tests/rdpmc/rdpmc_lib.c
@@ -9,6 +9,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/ioctl.h>
@@ -35,7 +36,7 @@
 
 #endif
 
-
+#if 0
 /* From Peter Zijlstra's demo code */
 unsigned long long mmap_read_self(void *addr,
 					 unsigned long long *enabled,
@@ -81,6 +82,100 @@ fail:
 	/* should do slow read here */
 	return -1;
 
+}
+#endif
+
+/* based on the code in include/uapi/linux/perf_event.h */
+inline unsigned long long mmap_read_self(void *addr,
+					 unsigned long long *en,
+					 unsigned long long *ru) {
+
+	struct perf_event_mmap_page *pc = addr;
+
+	uint32_t seq, time_mult, time_shift, index, width;
+	uint64_t count, enabled, running;
+	uint64_t cyc, time_offset;
+	int64_t pmc = 0;
+	uint64_t quot, rem;
+	uint64_t delta = 0;
+
+
+	do {
+		/* The kernel increments pc->lock any time */
+		/* perf_event_update_userpage() is called */
+		/* So by checking now, and the end, we */
+		/* can see if an update happened while we */
+		/* were trying to read things, and re-try */
+		/* if something changed */
+		/* The barrier ensures we get the most up to date */
+		/* version of the pc->lock variable */
+
+		seq=pc->lock;
+		barrier();
+
+		/* For multiplexing */
+		/* time_enabled is time the event was enabled */
+		enabled = pc->time_enabled;
+		/* time_running is time the event was actually running */
+		running = pc->time_running;
+
+		/* if cap_user_time is set, we can use rdtsc */
+		/* to calculate more exact enabled/running time */
+		/* for more accurate multiplex calculations */
+		if ( (pc->cap_user_time) && (enabled != running)) {
+			cyc = rdtsc();
+			time_offset = pc->time_offset;
+			time_mult = pc->time_mult;
+			time_shift = pc->time_shift;
+
+			quot=(cyc>>time_shift);
+			rem = cyc & (((uint64_t)1 << time_shift) - 1);
+			delta = time_offset + (quot * time_mult) +
+				((rem * time_mult) >> time_shift);
+		}
+		enabled+=delta;
+
+		/* actually do the measurement */
+
+		/* Index of register to read */
+		/* 0 means stopped/not-active */
+		/* Need to subtract 1 to get actual index to rdpmc() */
+		index = pc->index;
+
+		/* count is the value of the counter the last time */
+		/* the kernel read it */
+		count = pc->offset;
+
+		/* Ugh, libpfm4 perf_event.h has cap_usr_rdpmc */
+		/* while actual perf_event.h has cap_user_rdpmc */
+
+		/* Only read if rdpmc enabled and event index valid */
+		/* Otherwise return the older (out of date?) count value */
+		if (pc->cap_user_rdpmc && index) {
+			/* width can be used to sign-extend result */
+			width = pc->pmc_width;
+
+			/* Read counter value */
+			pmc = rdpmc(index-1);
+
+			/* sign extend result */
+			pmc<<=(64-width);
+			pmc>>=(64-width);
+
+			/* add current count into the existing kernel count */
+			count+=pmc;
+
+			running+=delta;
+		}
+
+		barrier();
+
+	} while (pc->lock != seq);
+
+	if (en) *en=enabled;
+	if (ru) *ru=running;
+
+	return count;
 }
 
 
