7b7c8a9a2eda43a241789f052853016fb08b893a
Vince Weaver <vincent.weaver@maine.edu>
2017-07-26 14:34:28 -0400

    tests: add rdpmc_exec_papi test
    if you exec without munmapping first then things get sad

--- run_tests.sh
+++ run_tests.sh
@@ -438,6 +438,9 @@ $TESTS_DIR/rdpmc/rdpmc_attach_multi_enable
 echo "  + $TESTS_DIR/rdpmc/rdpmc_exec"
 echo -n "    "
 $TESTS_DIR/rdpmc/rdpmc_exec
+echo "  + $TESTS_DIR/rdpmc/rdpmc_exec_papi"
+echo -n "    "
+$TESTS_DIR/rdpmc/rdpmc_exec_papi
 
 echo
 echo "* Checking OpenMP support"
