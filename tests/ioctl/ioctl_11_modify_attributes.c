fde2f37edef4369ae7563579a82972b7627f034e
Vince Weaver <vincent.weaver@maine.edu>
2018-07-10 15:07:15 -0400

    tests: ioctl_11 test actually tests ioctl_11 now

--- tests/ioctl/ioctl_11_modify_attributes.c
+++ tests/ioctl/ioctl_11_modify_attributes.c
@@ -1,7 +1,12 @@
-/* ioctl_0_enable.c				*/
-/* Test the PERF_IOC_ENABLE ioctl		*/
+/* ioctl_11_modify_attributes.c				*/
+/* Test the PERF_IOC_MODIFY_ATTRIBUTES ioctl		*/
 
-/* by Vince Weaver   vincent.weaver _at_ maine.edu  */
+/* Current only works for breakpoint events */
+/* Used to modify breakpoint address w/o opening/closing */
+
+/* Introduced Linux 4.17, 32ff77e8cc9e66cc4fb38098f64fd54cc8f54573 */
+
+/* by Vince Weaver   vincent.weaver@maine.edu		*/
 
 
 #include <stdio.h>
@@ -19,7 +24,7 @@
 #include "test_utils.h"
 #include "instructions_testcode.h"
 
-static char test_string[]="Testing ioctl(PERF_EVENT_IOC_ENABLE)...";
+static char test_string[]="Testing ioctl(PERF_EVENT_IOC_MODIFY_ATTRIBUTES)...";
 
 int main(int argc, char** argv) {
 
@@ -27,13 +32,14 @@ int main(int argc, char** argv) {
 	int quiet;
 	int read_result;
 	long long insn_data[4];
+	int result;
 
-	struct perf_event_attr pe;
+	struct perf_event_attr pe,pe2;
 
 	quiet=test_quiet();
 
 	if (!quiet) {
-		printf("This tests the PERF_EVENT_IOC_ENABLE ioctl.\n\n");
+		printf("This tests the PERF_EVENT_IOC_MODIFY_ATTRIBUTES ioctl.\n\n");
 	}
 
 	memset(&pe,0,sizeof(struct perf_event_attr));
@@ -43,7 +49,6 @@ int main(int argc, char** argv) {
 	pe.disabled=1;
 	pe.exclude_kernel=1;
 	pe.exclude_hv=1;
-	pe.read_format=PERF_FORMAT_ID;
 
 	arch_adjust_domain(&pe, quiet);
 
@@ -59,20 +64,42 @@ int main(int argc, char** argv) {
 
 	ioctl(fd[0], PERF_EVENT_IOC_DISABLE,0);
 
-	read_result=read(fd[0],insn_data,2*sizeof(long long));
+	read_result=read(fd[0],insn_data,sizeof(long long));
 
-	if (read_result!=2*sizeof(long long)) {
+	if (read_result!=sizeof(long long)) {
 		fprintf(stderr,"\tImproper return from read: %d\n",read_result);
 		test_fail(test_string);
 	}
 
-	close(fd[0]);
+	/* Attempt to modify non-breakpoint event */
+	/* This should fail */
 
-	if (!quiet) {
-		printf("\t%lld instructions\n",insn_data[0]);
+	memset(&pe2,0,sizeof(struct perf_event_attr));
+
+	pe2.type=PERF_TYPE_HARDWARE;
+	pe2.config=PERF_COUNT_HW_CPU_CYCLES;
+	pe2.disabled=1;
+	pe2.exclude_kernel=1;
+	pe2.exclude_hv=1;
+
+	arch_adjust_domain(&pe2, quiet);
+
+	result=ioctl(fd[0],PERF_EVENT_IOC_MODIFY_ATTRIBUTES,&pe2);
+	if (result<0) {
+		if (errno==EOPNOTSUPP) {
+			if (!quiet) {
+				printf("+ Failed as expected on non-breakpoint event\n");
+			}
+		}
+		else {
+			fprintf(stderr,"Unknown failure %s\n",
+				strerror(errno));
+			test_fail(test_string);
+		}
 	}
 
-	/* TODO -- test group leader vs not */
+
+	close(fd[0]);
 
 	test_pass(test_string);
 
