ed5b82abccaace6fec88298a6a0b74794c59d0ce
Vince Weaver <vincent.weaver@maine.edu>
2018-07-10 15:49:24 -0400

    tests: ioctl_11: modify_attributes: actually try to modify the attributes

--- tests/ioctl/ioctl_11_modify_attributes.c
+++ tests/ioctl/ioctl_11_modify_attributes.c
@@ -20,6 +20,8 @@
 #include <errno.h>
 
 #include "perf_event.h"
+#include "hw_breakpoint.h"
+
 #include "perf_helpers.h"
 #include "test_utils.h"
 #include "instructions_testcode.h"
@@ -98,6 +100,71 @@ int main(int argc, char** argv) {
 		}
 	}
 
+
+	close(fd[0]);
+
+	/*******************************/
+	/* Test execution breakpoint   */
+	/*******************************/
+
+	if (!quiet) {
+		printf("\tTesting HW_BREAKPOINT_X\n");
+	}
+
+	memset(&pe,0,sizeof(struct perf_event_attr));
+	pe.type=PERF_TYPE_BREAKPOINT;
+	pe.size=sizeof(struct perf_event_attr);
+
+	/* setup for an execution breakpoint */
+	pe.config=0;
+	pe.bp_type=HW_BREAKPOINT_X;
+	pe.bp_addr=(unsigned long)(main);
+	pe.bp_len=sizeof(long);
+
+	pe.disabled=1;
+	pe.exclude_kernel=1;
+	pe.exclude_hv=1;
+
+	arch_adjust_domain(&pe,quiet);
+
+	fd[0]=perf_event_open(&pe,0,-1,-1,0);
+	if (fd[0]<0) {
+		if (!quiet) {
+			printf("\t\tError opening leader %llx %s\n",
+				pe.config,strerror(errno));
+		}
+
+	}
+
+	ioctl(fd[0], PERF_EVENT_IOC_RESET, 0);
+	ioctl(fd[0], PERF_EVENT_IOC_ENABLE,0);
+
+
+	ioctl(fd[0], PERF_EVENT_IOC_DISABLE,0);
+
+
+	memset(&pe,0,sizeof(struct perf_event_attr));
+	pe.type=PERF_TYPE_BREAKPOINT;
+	pe.size=sizeof(struct perf_event_attr);
+
+	/* setup for an execution breakpoint */
+	pe.config=0;
+	pe.bp_type=HW_BREAKPOINT_X;
+	pe.bp_addr=(unsigned long)(main);
+	pe.bp_len=sizeof(long);
+
+	pe.disabled=1;
+	pe.exclude_kernel=1;
+	pe.exclude_hv=1;
+
+	arch_adjust_domain(&pe,quiet);
+
+	result=ioctl(fd[0],PERF_EVENT_IOC_MODIFY_ATTRIBUTES,&pe);
+	if (result<0) {
+		fprintf(stderr,"ioctl didn't work %s\n",
+			strerror(errno));
+		test_fail(test_string);
+	}
 
 	close(fd[0]);
 
