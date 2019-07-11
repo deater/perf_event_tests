88d81257c5c2f6087beb8a735225f68027e5500e
Vince Weaver <vincent.weaver@maine.edu>
2019-07-11 16:05:32 -0400

    perf_tool_fuzzer: initial checkin

---
+++ perf_tool_fuzzer/perf_tool_fuzzer.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int main(int argc, char **argv) {
+
+	int fd;
+
+	/* Open file */
+	fd=open("perf.data",O_WRONLY|O_CREAT);
+	if (fd<0) {
+		fprintf(stderr,"Error creating file: %s\n",
+			strerror(errno));
+		return -1;
+	}
+
+	/* FIXME: try messing with permissions */
+
+	/* First, a header */
+
+	close(fd);
+
+	return 0;
+
+}
