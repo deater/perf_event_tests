cd135885e52f4fbecad0e66d36d15e9a73f6a56a
Vince Weaver <vincent.weaver@maine.edu>
2026-08-18 15:43:41 -0400

    papi: update topdown zen5
    all L1 and L2

--- papi/papi_topdown.c
+++ papi/papi_topdown.c
@@ -1,5 +1,3 @@
-/* FIXME: raw event for missing one */
-
 /* Test out PAPI topdown support */
 
 /* at least originally just event-based on Zen5 */
@@ -47,7 +45,11 @@ static int zen5_level1(void) {
 
 	event_name1=strdup("CYCLES_NOT_IN_HALT:u=1:k=1");
 	event_name2=strdup("DISPATCH_STALLS_1:FE_NO_OPS:u=1:k=1");
-	event_name3=strdup("OPS_SOURCE_DISPATCHED_FROM_DECODER:DECODER:OPCACHE:k=1:u=1");
+
+	/* FIXME: missing field on libpfm4 */
+//	event_name3=strdup("OPS_SOURCE_DISPATCHED_FROM_DECODER:DECODER:OPCACHE:k=1:u=1");
+	event_name3=strdup("perf_raw::r4307aa:u=1:k=1");
+
 	event_name4=strdup("RETIRED_OPS:k=1:u=1");
 	event_name5=strdup("DISPATCH_STALLS_1:BE_STALLS:u=1:k=1");
 	event_name6=strdup("DISPATCH_STALLS_1:SMT_CONTENTION:u=1:k=1");
@@ -289,16 +291,19 @@ static int zen5_level2_frontend(void) {
 
 
 	long long total_dispatch_slots;
+	double frontend_bound;
 	double frontend_bound_latency;
 	double frontend_bound_bw;
 
 	total_dispatch_slots=8*total_1;
+	frontend_bound=(double)total_3/(double)total_dispatch_slots;
 	frontend_bound_latency=(double)8.0*(double)total_2/
 		(double)total_dispatch_slots;
 	frontend_bound_bw=(total_3-8*total_2)/(double)total_dispatch_slots;
 
 	if (!quiet) {
 		printf("\tTotal Dispatch Slots = %lld\n",total_dispatch_slots);
+		printf("\tFrontend Bound = %.2lf%%\n",frontend_bound*100.0);
 		printf("\tFrontend Bound Latency = %.2lf%%\n",frontend_bound_latency*100.0);
 		printf("\tFrontend Bound Bandwidth = %.2lf%%\n",frontend_bound_bw*100.0);
 	}
@@ -338,7 +343,8 @@ static int zen5_level2_bad_speculation(void) {
 	char *event_name5=NULL;
 
 	event_name1=strdup("CYCLES_NOT_IN_HALT:u=1:k=1");
-	event_name2=strdup("OPS_SOURCE_DISPATCHED_FROM_DECODER:DECODER:OPCACHE:k=1:u=1");
+//	event_name2=strdup("OPS_SOURCE_DISPATCHED_FROM_DECODER:DECODER:OPCACHE:k=1:u=1");
+	event_name2=strdup("perf_raw::r4307aa:u=1:k=1");
 	event_name3=strdup("RETIRED_OPS:k=1:u=1");
 	event_name4=strdup("RETIRED_BRANCH_INSTRUCTIONS_MISPREDICTED:u=1:k=1");
 	event_name5=strdup("BP_REDIRECTS:RESYNC:u=1:k=1");
@@ -464,6 +470,272 @@ static int zen5_level2_bad_speculation(void) {
 }
 
 
+/******************************************************/
+/******************************************************/
+/**************** Level2 Backend Bound ****************/
+/******************************************************/
+/******************************************************/
+
+static int zen5_level2_backend_bound(void) {
+
+	int retval;
+	int eventset=PAPI_NULL;
+
+	int quiet=0;
+	long long counts[4]={0,0,0,0};
+	long long total_1=0;
+	long long total_2=0;
+	long long total_3=0;
+	long long total_4=0;
+
+
+	char *event_name1=NULL;
+	char *event_name2=NULL;
+	char *event_name3=NULL;
+	char *event_name4=NULL;
+
+	event_name1=strdup("CYCLES_NOT_IN_HALT:u=1:k=1");
+	event_name2=strdup("DISPATCH_STALLS_1:BE_STALLS:u=1:k=1");
+		/* Fixme: missing completion filter */
+//	event_name3=strdup("CYCLES_NO_RETIRE:NOT_COMPLETE_LOAD_AND_ALU:u=1:k=1");
+	event_name3=strdup("perf_raw::r43a2d6:u=1:k=1");
+	event_name4=strdup("CYCLES_NO_RETIRE:NOT_COMPLETE_LOAD_AND_ALU:u=1:k=1");
+
+	printf("Level 2 Backend Bound\n");
+
+	/* create eventset */
+
+	retval=PAPI_create_eventset(&eventset);
+	if (retval!=PAPI_OK) {
+		if (!quiet) printf("Error creating eventset!\n");
+		fprintf(stderr,"Error creating eventset\n");
+		exit(-1);
+	}
+
+	/* Try to open event1 */
+	retval=PAPI_add_named_event(eventset,event_name1);
+	if (retval!=PAPI_OK) {
+		if (!quiet) printf("Error adding %s\n",event_name1);
+		printf("%s\n",PAPI_strerror(retval));
+		fprintf(stderr,"Error adding event1\n");
+		exit(-1);
+	}
+
+	/* Try to open event2 */
+	retval=PAPI_add_named_event(eventset,event_name2);
+	if (retval!=PAPI_OK) {
+		if (!quiet) printf("Error adding %s\n",event_name2);
+		printf("%s\n",PAPI_strerror(retval));
+		fprintf(stderr,"Error adding event2\n");
+		exit(-1);
+	}
+
+	/* Try to open event3 */
+	retval=PAPI_add_named_event(eventset,event_name3);
+	if (retval!=PAPI_OK) {
+		if (!quiet) printf("Error adding %s\n",event_name3);
+		printf("%s\n",PAPI_strerror(retval));
+		fprintf(stderr,"Error adding event3\n");
+		exit(-1);
+	}
+
+	/* Try to open event4 */
+	retval=PAPI_add_named_event(eventset,event_name4);
+	if (retval!=PAPI_OK) {
+		if (!quiet) printf("Error adding %s\n",event_name4);
+		printf("%s\n",PAPI_strerror(retval));
+		fprintf(stderr,"Error adding event4\n");
+		exit(-1);
+	}
+
+
+	if (!quiet) {
+		printf("\tSleeping 1s\n");
+	}
+
+	int start_result;
+	int stop_result;
+
+//	for(i=0;i<NUM_RUNS;i++) {
+		//printf("starting event\n");
+		start_result=PAPI_start(eventset);
+		if (start_result!=PAPI_OK) {
+			if (!quiet) printf("Error starting event %s!\n",
+				PAPI_strerror(start_result));
+			fprintf(stderr,"Error starting event\n");
+			exit(-1);
+
+		}
+
+		sleep(1);
+
+		//printf("stopping event\n");
+		stop_result=PAPI_stop(eventset,counts);
+		if (stop_result!=PAPI_OK) {
+			if (!quiet) printf("Error stopping event %s!\n",
+				PAPI_strerror(stop_result));
+			fprintf(stderr,"Error stopping event\n");
+			exit(-1);
+		}
+
+		total_1=counts[0];
+		total_2=counts[1];
+		total_3=counts[2];
+		total_4=counts[3];
+
+//	}
+
+
+	long long total_dispatch_slots;
+	double backend_bound;
+	double backend_bound_memory;
+	double backend_bound_cpu;
+
+	total_dispatch_slots=8*total_1;
+	backend_bound=(total_2)/(double)total_dispatch_slots;
+	backend_bound_memory=backend_bound*((double)total_3/total_4);
+	backend_bound_cpu=backend_bound*(1.0-((double)total_3/total_4));
+
+
+	if (!quiet) {
+//		printf("\traw: %lld %lld %lld %lld\n",total_1,total_2,total_3,total_4);
+		printf("\tTotal Dispatch Slots = %lld\n",total_dispatch_slots);
+		printf("\tBackend Bound = %.2lf%%\n",backend_bound*100.0);
+		printf("\tBackend Bound Memory = %.2lf%%\n",backend_bound_memory*100.0);
+		printf("\tBackend Bound CPU = %.2lf%%\n",backend_bound_cpu*100.0);
+	}
+
+	if (!quiet) printf("\n");
+
+	PAPI_destroy_eventset(&eventset);
+
+	return 0;
+
+}
+
+
+/******************************************************/
+/******************************************************/
+/**************** Level 2 Retiring  *******************/
+/******************************************************/
+/******************************************************/
+
+static int zen5_level2_retiring(void) {
+
+	int retval;
+	int eventset=PAPI_NULL;
+
+	int quiet=0;
+	long long counts[3]={0,0,0};
+	long long total_1=0;
+	long long total_2=0;
+	long long total_3=0;
+
+	char *event_name1=NULL;
+	char *event_name2=NULL;
+	char *event_name3=NULL;
+
+	event_name1=strdup("CYCLES_NOT_IN_HALT:u=1:k=1");
+	event_name2=strdup("RETIRED_OPS:k=1:u=1");
+	event_name3=strdup("RETIRED_UCODE_OPS:k=1:u=1");
+
+	printf("Level 2 Retiring\n");
+
+	/* create eventset */
+
+	retval=PAPI_create_eventset(&eventset);
+	if (retval!=PAPI_OK) {
+		if (!quiet) printf("Error creating eventset!\n");
+		fprintf(stderr,"Error creating eventset\n");
+		exit(-1);
+	}
+
+	/* Try to open event1 */
+	retval=PAPI_add_named_event(eventset,event_name1);
+	if (retval!=PAPI_OK) {
+		if (!quiet) printf("Error adding %s\n",event_name1);
+		printf("%s\n",PAPI_strerror(retval));
+		fprintf(stderr,"Error adding event1\n");
+		exit(-1);
+	}
+
+	/* Try to open event2 */
+	retval=PAPI_add_named_event(eventset,event_name2);
+	if (retval!=PAPI_OK) {
+		if (!quiet) printf("Error adding %s\n",event_name2);
+		printf("%s\n",PAPI_strerror(retval));
+		fprintf(stderr,"Error adding event2\n");
+		exit(-1);
+	}
+
+	/* Try to open event3 */
+	retval=PAPI_add_named_event(eventset,event_name3);
+	if (retval!=PAPI_OK) {
+		if (!quiet) printf("Error adding %s\n",event_name3);
+		printf("%s\n",PAPI_strerror(retval));
+		fprintf(stderr,"Error adding event3\n");
+		exit(-1);
+	}
+
+	if (!quiet) {
+		printf("\tSleeping 1s\n");
+	}
+
+	int start_result;
+	int stop_result;
+
+//	for(i=0;i<NUM_RUNS;i++) {
+		//printf("starting event\n");
+		start_result=PAPI_start(eventset);
+		if (start_result!=PAPI_OK) {
+			if (!quiet) printf("Error starting event %s!\n",
+				PAPI_strerror(start_result));
+			fprintf(stderr,"Error starting event\n");
+			exit(-1);
+
+		}
+
+		sleep(1);
+
+		//printf("stopping event\n");
+		stop_result=PAPI_stop(eventset,counts);
+		if (stop_result!=PAPI_OK) {
+			if (!quiet) printf("Error stopping event %s!\n",
+				PAPI_strerror(stop_result));
+			fprintf(stderr,"Error stopping event\n");
+			exit(-1);
+		}
+
+		total_1=counts[0];
+		total_2=counts[1];
+		total_3=counts[2];
+//	}
+
+
+	long long total_dispatch_slots;
+	double retiring;
+	double retiring_fastpath;
+	double retiring_microcode;
+
+	total_dispatch_slots=8*total_1;
+	retiring=(total_2)/(double)total_dispatch_slots;
+	retiring_fastpath=retiring*(total_2-total_3)/(double)(total_2);
+	retiring_microcode=retiring*(total_3)/(double)(total_2);
+
+	if (!quiet) {
+		printf("\tRetiring = %.2lf%%\n",retiring*100.0);
+		printf("\tRetiring Fastpath = %.2lf%%\n",retiring_fastpath*100.0);
+		printf("\tRetiring Microcode = %.2lf%%\n",retiring_microcode*100.0);
+	}
+
+	if (!quiet) printf("\n");
+
+	PAPI_destroy_eventset(&eventset);
+
+	return 0;
+
+}
+
 
 int main(int argc, char **argv) {
 
@@ -506,6 +778,8 @@ int main(int argc, char **argv) {
 	zen5_level1();
 	zen5_level2_frontend();
 	zen5_level2_bad_speculation();
+	zen5_level2_backend_bound();
+	zen5_level2_retiring();
 
 	PAPI_shutdown();
 
